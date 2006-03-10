74b347da094e117417a49a15d8d6fd0a1e0de975
Sven Kralisch <sven@kralisch.com>
2006-03-10 11:46:41 +0000

    made a copy
    git-svn-id: http://svn.geogr.uni-jena.de/svn/jamsmodels/devel@226 503a54e1-1403-0410-a754-803ee147b016

---
+++ CROP/src/org/crop/StandardFertParaReader.java
@@ -0,0 +1,94 @@
+/*
+ * StandardFertParaReader.java
+ * used to describe fertilization options
+ * Created on 9. Dezember 2005, 15:37
+ *
+ * This file is part of JAMS
+ * Copyright (C) 2005 FSU Jena, c5ulbe
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ *
+ */
+
+package org.crop;
+
+import org.unijena.jams.data.*;
+import org.unijena.jams.model.*;
+import java.util.*;
+import java.io.*;
+
+/**
+ *
+ * @author S. Kralisch file edited by U. Bende-Michl
+ */
+public class StandardFertParaReader extends JAMSComponent {
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            update = JAMSVarDescription.UpdateType.INIT,
+            description = "Data file directory name"
+            )
+            public JAMSString dirName;
+
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            update = JAMSVarDescription.UpdateType.INIT,
+            description = "Fertilizer parameter file name"
+            )
+            public JAMSString ftFileName;
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            update = JAMSVarDescription.UpdateType.INIT,
+            description = "Collection of hru objects"
+            )
+            public JAMSEntityCollection hrus;
+    
+    
+    
+    public void init() throws JAMSEntity.NoSuchAttributeException {
+        
+        //read fertilizer parameter
+        JAMSEntityCollection fert = new JAMSEntityCollection();
+        fert.setEntities(J2KFunctions.readParas(dirName.getValue()+"/"+ftFileName.getValue()));
+        
+        HashMap<Double, JAMSEntity> ftMap = new HashMap<Double, JAMSEntity>();
+        JAMSEntity ft, e;
+        Object[] attrs;
+        
+        //put all entities into a HashMap with their ID as key
+        
+        Iterator<JAMSEntity> ftIterator = fert.getEntities().iterator();
+        while (ftIterator.hasNext()) {
+            ft = ftIterator.next();
+            ftMap.put(ft.getDouble("ID"),  ft);//put all entities into a HashMap with their ID as key
+                            }
+        
+        Iterator<JAMSEntity> hruIterator = hrus.getEntities().iterator();
+        while (hruIterator.hasNext()) {
+            e = hruIterator.next();
+            ft = ftMap.get(e.getDouble("fertID"));
+            e.setObject("fert", ft);
+            
+            attrs = ft.getDoubleKeys();
+            for (int i = 0; i < attrs.length; i++) {
+                e.setDouble((String) attrs[i], ft.getDouble((String) attrs[i]));
+            }
+            
+        }
+    }
+      
+}
+
---
+++ CROP/src/org/crop/StandardTillParaReader.java
@@ -0,0 +1,94 @@
+/*
+ * StandardTillParaReader.java
+ * used to describe tillage options
+ Created on 9. Dezember 2005, 15:46
+ *
+ * This file is part of JAMS
+ * Copyright (C) 2005 FSU Jena, c5ulbe
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ *
+ */
+
+package org.crop;
+
+import org.unijena.jams.data.*;
+import org.unijena.jams.model.*;
+import java.util.*;
+import java.io.*;
+
+/**
+ *
+ * @author S. Kralisch file edited by U. Bende-Michl
+ */
+public class StandardTillParaReader extends JAMSComponent {
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            update = JAMSVarDescription.UpdateType.INIT,
+            description = "Data file directory name"
+            )
+            public JAMSString dirName;
+
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            update = JAMSVarDescription.UpdateType.INIT,
+            description = "Tillage parameter file name"
+            )
+            public JAMSString tlFileName;
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            update = JAMSVarDescription.UpdateType.INIT,
+            description = "Collection of hru objects"
+            )
+            public JAMSEntityCollection hrus;
+    
+    
+    
+    public void init() throws JAMSEntity.NoSuchAttributeException {
+        
+        //read tillage parameter
+        JAMSEntityCollection till = new JAMSEntityCollection();
+        till.setEntities(J2KFunctions.readParas(dirName.getValue()+"/"+tlFileName.getValue()));
+        
+        HashMap<Double, JAMSEntity> tlMap = new HashMap<Double, JAMSEntity>();
+        JAMSEntity tl, e;
+        Object[] attrs;
+        
+        //put all entities into a HashMap with their ID as key
+        
+        Iterator<JAMSEntity> tlIterator = till.getEntities().iterator();
+        while (tlIterator.hasNext()) {
+            tl = tlIterator.next();
+            tlMap.put(tl.getDouble("ID"),  tl);//put all entities into a HashMap with their ID as key
+                            }
+        
+        Iterator<JAMSEntity> hruIterator = hrus.getEntities().iterator();
+        while (hruIterator.hasNext()) {
+            e = hruIterator.next();
+            tl = tlMap.get(e.getDouble("tillID"));
+            e.setObject("till", tl);
+            
+            attrs = tl.getDoubleKeys();
+            for (int i = 0; i < attrs.length; i++) {
+                e.setDouble((String) attrs[i], tl.getDouble((String) attrs[i]));
+            }
+            
+        }
+    }
+      
+}
+
