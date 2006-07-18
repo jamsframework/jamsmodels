f50ee7c1a26e1bce64aa2eb891f9143b76387a65
Manfred Fink <manfred.fink@uni-jena.de>
2006-07-18 14:04:29 +0000

    git-svn-id: http://svn.geogr.uni-jena.de/svn/jamsmodels/devel@478 503a54e1-1403-0410-a754-803ee147b016

--- J2K_S_N/src/org/jams/j2k/s_n/J2KPlantGrowthNitrogenStress.java
+++ J2K_S_N/src/org/jams/j2k/s_n/J2KPlantGrowthNitrogenStress.java
@@ -86,7 +86,7 @@ import org.unijena.jams.model.*;
         double phi_nit = 0; // scaling factor for nitrogen stress [-]
         
         run_actbioN = run_actbioN + runactN_up;
-        phi_nit = 200 * ((run_actbioN / run_optibioN) - 0.5);
+        phi_nit = 200 * (((run_actbioN + 0.1) / (run_optibioN + 0.1)) - 0.5);
         
         run_nstrs = 1 - (phi_nit / (phi_nit * Math.exp(3.535 - (0.02597 * phi_nit))));
         actbioN.setValue(run_actbioN);
