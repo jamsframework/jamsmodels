fb3700faaa88922a6fcee7d42edf7eb74daceb2b
Peter Krause <p.krause@uni-jena.de>
2007-03-01 07:45:50 +0000

    git-svn-id: http://svn.geogr.uni-jena.de/svn/jamsmodels/devel@721 503a54e1-1403-0410-a754-803ee147b016

---
+++ JAMSScs/src/org/unijena/scs/CalcRetentionFactors.java
@@ -0,0 +1,107 @@
+/*
+ * CalcRetentionFactors.java
+ * Created on 17. July 2006, 17:15
+ *
+ * This file is part of JAMS
+ * Copyright (C) 2005 S. Kralisch and P. Krause
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ *
+ */
+package org.unijena.scs;
+import org.unijena.jams.data.JAMSDouble;
+import org.unijena.jams.data.JAMSEntity;
+import org.unijena.jams.model.JAMSComponent;
+import org.unijena.jams.model.JAMSComponentDescription;
+import org.unijena.jams.model.JAMSVarDescription;
+
+/**
+ *
+ * @author P. Krause
+ */
+
+@JAMSComponentDescription(
+        title="Calc retention factors",
+        author="Peter Krause",
+        description="Calculates the recession coefficients k1 and k2" +
+        "for the unit hydrographs, based on a length-slope factor of the" +
+        "main stream in the catchment." +
+        "In addition, the factor beta for the distribution of the effective" +
+        "precipitation for the two unit-hydrographs is calculated here, also" +
+        "based on the length-slope factor"
+        )
+public class CalcRetentionFactors extends JAMSComponent {
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READWRITE,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "distribution factor beta"
+            )
+            public JAMSDouble beta;
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READWRITE,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "recession factor k1"
+            )
+            public JAMSDouble k1;
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READWRITE,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "recession factor k2"
+            )
+            public JAMSDouble k2;
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "stream slope"
+            )
+            public JAMSDouble streamSlope;
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "stream length",
+            unit = "km^2"
+            )
+            public JAMSDouble streamLength;
+    
+    /* (non-Javadoc)
+     * @see org.unijena.jams.model.JAMSComponent#run()
+     */
+    public void run() throws JAMSEntity.NoSuchAttributeException {
+        /*the length slope factor of the catchment */
+        double lengthSlope = this.streamLength.getValue() / Math.sqrt(this.streamSlope.getValue());
+        
+        /*factor beta for precip distribution*/
+        double beta = 0;
+        if(lengthSlope < 10.0){
+            beta = 1 - 0.02425 * Math.pow(Math.log(lengthSlope),3.2444);
+        }else{
+            beta = 0.1 + 3.91 / (Math.pow(lengthSlope, 0.86));
+        }
+        
+        /*the recession coefficients k1 and k2 */
+        double k1 = 0.555 / Math.pow(lengthSlope, 0.61) + 0.511 * Math.log(lengthSlope) - 0.355;
+        double k2 = 3 * Math.pow(k1, 1.3);
+        
+        this.beta.setValue(beta);
+        this.k1.setValue(k1);
+        this.k2.setValue(k2);
+    }
+    
+}
---
+++ JAMSScs/src/org/unijena/scs/ParallelStorageCascade.java
@@ -0,0 +1,190 @@
+/*
+ * ParallelStorageCascade.java
+ * Created on 17. July 2006, 17:15
+ *
+ * This file is part of JAMS
+ * Copyright (C) 2005 S. Kralisch and P. Krause
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ *
+ */
+package org.unijena.scs;
+import org.unijena.jams.model.*;
+import org.unijena.jams.data.*;
+
+/**
+ *
+ * @author P. Krause
+ */
+@JAMSComponentDescription(
+        title="Parallel storage cascade",
+        author="Peter Krause",
+        description="simulates runoff in a parallel linear storage cascade"
+        )
+public class ParallelStorageCascade extends JAMSComponent {
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "time"
+            )
+            public JAMSCalendar time;
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            update = JAMSVarDescription.UpdateType.INIT,
+            description = "timeInterval"
+            )
+            public JAMSTimeInterval timeInterval;
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            update = JAMSVarDescription.UpdateType.RUN,
+            unit = "km^2",
+            description = "the entire area of the catchment"
+            )
+            public JAMSDouble catchmentArea;
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.WRITE,
+            update = JAMSVarDescription.UpdateType.RUN,
+            unit = "m³/s",
+            description = "runoff"
+            )
+            public JAMSDouble runoff;
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.WRITE,
+            update = JAMSVarDescription.UpdateType.RUN,
+            unit = "m³/s",
+            description = "max runoff"
+            )
+            public JAMSDouble maxRunoff;
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.WRITE,
+            update = JAMSVarDescription.UpdateType.RUN,
+            unit = "mm",
+            description = "volume"
+            )
+            public JAMSDouble volume;
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.WRITE,
+            update = JAMSVarDescription.UpdateType.RUN,
+            unit = "m³/s",
+            description = "runoff_arr"
+            )
+            public JAMSDoubleArray runoff_arr;
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.WRITE,
+            update = JAMSVarDescription.UpdateType.RUN,
+            unit = "mm",
+            description = "volume_arr"
+            )
+            public JAMSDoubleArray volume_arr;
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "unit hydrograph 1"
+            )
+            public JAMSDoubleArray uh1;
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "unit hydrograph 2"
+            )
+            public JAMSDoubleArray uh2;
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "precip for uh1"
+            )
+            public JAMSDoubleArray hNe1;
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "precip for uh2"
+            )
+            public JAMSDoubleArray hNe2;
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "array length"
+            )
+            public JAMSInteger arrayLength;
+    
+    
+    double[] u1_arr, u2_arr; 
+    double[] vol_arr, run_arr;
+    int timeStepCounter = 0;
+    int start = 0;
+    
+    public void init() throws JAMSEntity.NoSuchAttributeException {
+        this.timeStepCounter = 0;
+        int timeSteps = (int)timeInterval.getNumberOfTimesteps()+1;
+        vol_arr = new double[timeSteps]; 
+        run_arr = new double[timeSteps];
+        this.maxRunoff.setValue(0);
+        this.volume.setValue(0);
+    } 
+    
+    public void run() throws JAMSEntity.NoSuchAttributeException {
+        //let it run until no relevant outflow occurs
+    	this.u1_arr = this.uh1.getValue();
+        this.u2_arr = this.uh2.getValue();
+        
+        //let it run until no relevant outflow occurs
+        double runoff = 0;
+        double volume = this.volume.getValue();
+        int arrayLength = this.arrayLength.getValue();
+       
+        if(this.timeStepCounter < arrayLength){
+            start = 0;
+        } else{
+            start++;
+        }
+        int pIdx = this.timeStepCounter;
+        
+        for(int j = 0; j <= this.timeStepCounter; j++){
+            runoff = runoff + u1_arr[j] * hNe1.getValue()[pIdx];
+            runoff = runoff + u2_arr[j] * hNe2.getValue()[pIdx];
+            
+            pIdx = pIdx - 1;
+        }
+        volume = volume + (runoff * this.timeInterval.getTimeUnitCount());
+        
+        run_arr[this.timeStepCounter] = runoff;
+        vol_arr[this.timeStepCounter] = volume;
+        
+        if(runoff > this.maxRunoff.getValue())
+            this.maxRunoff.setValue(runoff);
+        
+        this.runoff.setValue(runoff);
+        this.volume.setValue(volume);
+        
+        this.runoff_arr.setValue(run_arr);
+        
+        runoff = 0;
+        this.timeStepCounter++;
+        
+    }
+}
