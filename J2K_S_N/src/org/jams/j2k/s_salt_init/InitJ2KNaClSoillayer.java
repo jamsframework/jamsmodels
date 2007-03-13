bd005d1ebe03af990a31b0a7915b472e354c9e3a
Manfred Fink <manfred.fink@uni-jena.de>
2007-03-13 09:37:12 +0000

    git-svn-id: http://svn.geogr.uni-jena.de/svn/jamsmodels/devel@731 503a54e1-1403-0410-a754-803ee147b016

---
+++ J2K_S_N/src/org/jams/j2k/s_salt_init/InitJ2KNaClSoillayer.java
@@ -0,0 +1,255 @@
+/*
+ * InitJ2KNaClSoillayer.java
+ * Created on 17. February 2006, 14:49
+ *
+ * This file is part of JAMS
+ * Copyright (C) 2006 FSU Jena, Manfred Fink
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ *
+ */
+
+package org.jams.j2k.s_salt_init;
+
+import org.unijena.jams.data.*;
+import org.unijena.jams.model.*;
+
+/**
+ *
+ * @author Manfred Fink
+ */
+@JAMSComponentDescription(
+        title="InitJ2KNaClSoillayer",
+        author="Manfred Fink",
+        description="intitiallizing NaCl Pools in Soil and additional variables"
+        )
+        public class InitJ2KNaClSoillayer extends JAMSComponent  {
+    
+    /*
+     *  Component variables
+     */
+    
+    
+    
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "in cm depth of soil layer"
+            )
+            public JAMSDoubleArray layerdepth = new JAMSDoubleArray();
+    
+    
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "in kg/dm³ soil bulk density"
+            )
+            public JAMSDoubleArray soil_bulk_density = new JAMSDoubleArray();
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = " in % organic Carbon in soil"
+            )
+            public JAMSDoubleArray C_org = new JAMSDoubleArray();
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READWRITE,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = " NO3-Pool in kgN/ha"
+            )
+            public JAMSDoubleArray NaCl_Pool = new JAMSDoubleArray();
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READWRITE,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = " Residue in Layer in kgN/ha"
+            )
+            public JAMSDoubleArray Residue_pool = new JAMSDoubleArray();
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READWRITE,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = " N-Organic fresh Pool from Residue in kgN/ha"
+            )
+            public JAMSDoubleArray NaCl_residue_pool_fresh = new JAMSDoubleArray();
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READWRITE,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = " Nitrate in interflow in added to HRU layer in kgN"
+            )
+            public JAMSDoubleArray InterflowNaCl_in  = new JAMSDoubleArray();
+   
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.WRITE,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = " Input of plant residues kg/ha"
+            )
+            public JAMSDouble inp_biomass;
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.WRITE,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "Nitrogen input of plant residues in kgN/ha"
+            )
+            public JAMSDouble inpNaCl_biomass;
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "number of layers in soil profile in [-]"
+            )
+            public JAMSDouble Layer;
+    
+     @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.WRITE,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "flag plant existing yes or no " // attention its a boolean!
+            )
+            public JAMSBoolean plantExisting = new JAMSBoolean();
+     
+      @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.WRITE,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = " state variable LAI in [-]"
+            )
+            public JAMSDouble LAI;
+         
+       @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.WRITE,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "Actual rooting depth [dm]"
+            )
+            public JAMSDouble ZRootD;
+    
+      @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.WRITE,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "actual salt uptake by plants in kgNaCl/ha"
+            )
+            public JAMSDouble actNaCl_up;
+    
+    /*
+     *  Component run stages
+     */
+    
+    
+    
+    
+    
+    public void init() throws JAMSEntity.NoSuchAttributeException{
+        
+        
+    }
+    
+    public void run() throws JAMSEntity.NoSuchAttributeException{
+        int i = 0;
+        
+        int layer = (int)Layer.getValue();
+        double runlayerdepth;
+        plantExisting.setValue(true);
+        double runsoil_bulk_density;
+        
+        double runC_org;
+        
+        double hor_dept = 0;
+        
+        double runNaCl_Pool;
+        double[] NaCl_Poolvals = new double[layer];
+        
+        double runNaCl_residue_pool_fresh;
+        double[] NaCl_residue_pool_freshvals = new double[layer];
+        
+        double runResidue_pool;
+        double[] Residue_poolvals = new double[layer];
+        
+        double[] InterflowNaCl_invals = new double[layer];
+        
+        
+ 
+        
+        
+        
+        
+        while (i < layer){
+            
+            runC_org = C_org.getValue()[i];
+            runsoil_bulk_density = soil_bulk_density.getValue()[i];
+            runlayerdepth = layerdepth.getValue()[i] * 10; //from cm to mm
+            hor_dept = hor_dept + runlayerdepth;
+            runResidue_pool = 10;
+            runNaCl_Pool = ((7 * Math.exp(-hor_dept/1000)) * runsoil_bulk_density * runlayerdepth)/1000;
+            
+            
+            runNaCl_residue_pool_fresh = 0.0015 * runResidue_pool;
+            NaCl_Poolvals[i] = runNaCl_Pool;           
+           
+            Residue_poolvals[i] = runResidue_pool;
+            NaCl_residue_pool_freshvals[i] = runNaCl_residue_pool_fresh;
+            InterflowNaCl_invals[i] = 0;
+            
+            
+            i++;
+        }
+        
+        NaCl_Pool.setValue(NaCl_Poolvals);
+        Residue_pool.setValue(Residue_poolvals);
+        NaCl_residue_pool_fresh.setValue(NaCl_residue_pool_freshvals);
+        InterflowNaCl_in.setValue(InterflowNaCl_invals);
+        inp_biomass.setValue(0);
+        inpNaCl_biomass.setValue(0);
+        LAI.setValue(0);
+        ZRootD.setValue(0);
+        actNaCl_up.setValue(0);
+    }
+    
+    
+    public void cleanup() throws JAMSEntity.NoSuchAttributeException{
+        
+    }
+}
+/*
+                <component class="org.jams.j2k.s_n.init.initJ2KNSoil" name="initJ2KNSoil">
+                        <jamsvar name="NO3_Pool" provider="InitHRUContext" providervar="currentEntity.NO3_Pool"/>
+                        <jamsvar name="NH4_Pool" provider="InitHRUContext" providervar="currentEntity.NH4_Pool"/>
+                        <jamsvar name="N_activ_pool" provider="InitHRUContext" providervar="currentEntity.N_activ_pool"/>
+                        <jamsvar name="N_stabel_pool" provider="InitHRUContext" providervar="currentEntity.N_stabel_pool"/>
+                        <jamsvar name="Residue_pool" provider="InitHRUContext" providervar="currentEntity.residue_pool"/>
+                        <jamsvar name="N_residue_pool_fresh" provider="InitHRUContext" providervar="currentEntity.N_residue_pool_fresh"/>
+                        <jamsvar name="Volati_trans" provider="InitHRUContext" providervar="currentEntity.Volati_rate"/>
+                        <jamsvar name="NH4inp" provider="InitHRUContext" providervar="currentEntity.NH4inp"/>
+                        <jamsvar name="PlantupN" provider="InitHRUContext" providervar="currentEntity.PlantupN"/>
+                        <jamsvar name="Nitri_trans" provider="InitHRUContext" providervar="currentEntity.Nitri_rate"/>
+                        <jamsvar name="Denit_trans" provider="InitHRUContext" providervar="currentEntity.Denit_rate"/>
+                        <jamsvar name="SurfaceN" provider="InitHRUContext" providervar="currentEntity.SurfaceN"/>
+                        <jamsvar name="InterflowN" provider="InitHRUContext" providervar="currentEntity.InterflowN"/>
+                        <jamsvar name="PercoN" provider="InitHRUContext" providervar="currentEntity.PercoN"/>
+                        <jamsvar name="SurfaceNabs" provider="InitHRUContext" providervar="currentEntity.SurfaceNabs"/>
+                        <jamsvar name="InterflowNabs" provider="InitHRUContext" providervar="currentEntity.InterflowNabs"/>
+                        <jamsvar name="PercoNabs" provider="InitHRUContext" providervar="currentEntity.PercoNabs"/>
+                        <jamsvar name="SurfaceN_in" provider="InitHRUContext" providervar="currentEntity.SurfaceN_in"/>
+                        <jamsvar name="InterflowN_in" provider="InitHRUContext" providervar="currentEntity.InterflowN_in"/>
+                        <jamsvar name="PercoN_in" provider="InitHRUContext" providervar="currentEntity.PercoN_in"/>
+ 
+                        <jamsvar name="layerdepth" provider="InitHRUContext" providervar="currentEntity.rootDepth"/>
+                        <jamsvar name="totaldepth" provider="InitHRUContext" providervar="currentEntity.rootDepth"/>
+                        <jamsvar name="soil_bulk_density" value="1.3"/>
+                        <jamsvar name="C_org" value="1.5"/>
+                </component>
+ */
