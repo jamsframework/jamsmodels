6eaf8946f2c54654c04307bc932df700dcd035b8
Manfred Fink <manfred.fink@uni-jena.de>
2008-08-25 08:44:17 +0000

    git-svn-id: http://svn.geogr.uni-jena.de/svn/jamsmodels/trunk@893 503a54e1-1403-0410-a754-803ee147b016

---
+++ J2K_S_N/src/org/jams/j2k/s_n/io/soillayer_writer4.java
@@ -0,0 +1,250 @@
+/*
+ * soillayer_writer4.java
+ * Created on 12. März 2008, 11:43
+ *
+ * This file is part of JAMS
+ * Copyright (C) 2005 FSU Jena, c8fima
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ *
+ */
+
+package org.jams.j2k.s_n.io;
+
+import org.unijena.jams.data.*;
+import org.unijena.jams.model.*;
+
+/**
+ *
+ * @author c8fima
+ */
+@JAMSComponentDescription(
+title="soillayer_writer4",
+        author="Manfred Fink",
+        description="Writes array values of the soil storages for the first 4 soil layers. If layers are not existing values are 0"
+        )
+        public class soillayer_writer4 extends JAMSComponent {
+    
+    /*
+     *  Component variables
+     */
+    
+    @JAMSVarDescription(
+    access = JAMSVarDescription.AccessType.READ,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "in cm depth of soil layer"
+            )
+            public JAMSDoubleArray layerdepth = new JAMSDoubleArray();
+    
+    @JAMSVarDescription(
+    access = JAMSVarDescription.AccessType.READ,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "HRU area in m^2"
+            )
+            public JAMSDouble area;
+    
+    @JAMSVarDescription(
+    access = JAMSVarDescription.AccessType.READ,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "HRU attribute maximum FPS in mm"
+            )
+            public JAMSDoubleArray maxFPS = new JAMSDoubleArray();
+    
+    @JAMSVarDescription(
+    access = JAMSVarDescription.AccessType.READ,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "HRU attribute maximum MPS in mm"
+            )
+            public JAMSDoubleArray maxMPS = new JAMSDoubleArray();
+    
+    @JAMSVarDescription(
+    access = JAMSVarDescription.AccessType.READ,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "HRU attribute maximum LPS in mm"
+            )
+            public JAMSDoubleArray maxLPS = new JAMSDoubleArray();
+    
+    @JAMSVarDescription(
+    access = JAMSVarDescription.AccessType.READ,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "HRU state var actual MPS in mm"
+            )
+            public JAMSDoubleArray actMPS = new JAMSDoubleArray();
+    
+    @JAMSVarDescription(
+    access = JAMSVarDescription.AccessType.READ,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "HRU state var actual LPS in mm"
+            )
+            public JAMSDoubleArray actLPS = new JAMSDoubleArray();
+    
+    @JAMSVarDescription(
+    access = JAMSVarDescription.AccessType.WRITE,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "HRU state var actual LPS of the frist layer in mm"
+            )
+            public JAMSDouble actLPS1;
+    
+    @JAMSVarDescription(
+    access = JAMSVarDescription.AccessType.WRITE,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "HRU state var actual LPS of the second layer in mm"
+            )
+            public JAMSDouble actLPS2;
+    
+    @JAMSVarDescription(
+    access = JAMSVarDescription.AccessType.WRITE,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "HRU state var actual LPS of the third layer in mm"
+            )
+            public JAMSDouble actLPS3;
+    
+    @JAMSVarDescription(
+    access = JAMSVarDescription.AccessType.WRITE,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "HRU state var actual LPS of the fourth layer in mm"
+            )
+            public JAMSDouble actLPS4;
+    
+    @JAMSVarDescription(
+    access = JAMSVarDescription.AccessType.WRITE,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "HRU state var actual MPS of the frist layer in mm"
+            )
+            public JAMSDouble actMPS1;
+    
+    @JAMSVarDescription(
+    access = JAMSVarDescription.AccessType.WRITE,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "HRU state var actual MPS of the second layer in mm"
+            )
+            public JAMSDouble actMPS2;
+    
+    @JAMSVarDescription(
+    access = JAMSVarDescription.AccessType.WRITE,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "HRU state var actual MPS of the third layer in mm"
+            )
+            public JAMSDouble actMPS3;
+    
+    @JAMSVarDescription(
+    access = JAMSVarDescription.AccessType.WRITE,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "HRU state var actual MPS of the fourth layer in mm"
+            )
+            public JAMSDouble actMPS4;
+    
+    @JAMSVarDescription(
+    access = JAMSVarDescription.AccessType.WRITE,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "HRU soil moistrure of the frist layer in %"
+            )
+            public JAMSDouble actMoist1;
+    
+    @JAMSVarDescription(
+    access = JAMSVarDescription.AccessType.WRITE,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "HRU soil moistrure of the second layer in %"
+            )
+            public JAMSDouble actMoist2;
+    
+    @JAMSVarDescription(
+    access = JAMSVarDescription.AccessType.WRITE,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "HRU soil moistrure of the third layer in %"
+            )
+            public JAMSDouble actMoist3;
+    
+    @JAMSVarDescription(
+    access = JAMSVarDescription.AccessType.WRITE,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "HRU soil moistrure of the fourth layer in %"
+            )
+            public JAMSDouble actMoist4;
+    
+    @JAMSVarDescription(
+    access = JAMSVarDescription.AccessType.WRITE,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "HRU soil moistrure of the fourth layer in %"
+            )
+            public JAMSDoubleArray actMoist_h = new JAMSDoubleArray();
+    
+    
+    
+    
+    /*
+     *  Component run stages
+     */
+    double[] run_maxMPS, run_maxLPS, run_actMPS, run_actLPS, run_maxFPS, run_layerdepth;
+    public void init() {
+        
+    }
+    
+    public void run() {
+        
+        run_layerdepth = layerdepth.getValue();
+        run_maxFPS =  maxFPS.getValue();
+        run_maxMPS =  maxMPS.getValue();
+        run_maxLPS =  maxLPS.getValue();
+        run_actMPS =  actMPS.getValue();
+        run_actLPS =  actLPS.getValue();
+        
+        
+        int nhor = run_layerdepth.length;
+        
+        double[] run_actMoist_h = new double[nhor];
+        
+        actMPS1.setValue(run_actMPS[0]);
+        actLPS1.setValue(run_actLPS[0]);
+        actMoist1.setValue(((run_actMPS[0] + run_actLPS[0] + run_maxFPS[0])/(run_layerdepth[0]*10*area.getValue()))*100);
+        
+        if (nhor > 1){
+            actMPS2.setValue(run_actMPS[1]);
+            actLPS2.setValue(run_actLPS[1]);
+            actMoist2.setValue(((run_actMPS[1] + run_actLPS[1] + run_maxFPS[1])/(run_layerdepth[1]*10*area.getValue()))*100);
+        }else{
+            actMPS2.setValue(0);
+            actLPS2.setValue(0);
+            actMoist2.setValue(0);}
+        
+        if (nhor > 2){
+            actMPS3.setValue(run_actMPS[2]);
+            actLPS3.setValue(run_actLPS[2]);
+            actMoist3.setValue(((run_actMPS[2] + run_actLPS[2] + run_maxFPS[2])/(run_layerdepth[2]*10*area.getValue()))*100);
+        }else{
+            actMPS3.setValue(0);
+            actLPS3.setValue(0);
+            actMoist3.setValue(0);}
+        
+        if (nhor > 3){
+            actMPS4.setValue(run_actMPS[3]);
+            actLPS4.setValue(run_actLPS[3]);
+            actMoist4.setValue(((run_actMPS[3] + run_actLPS[3] + run_maxFPS[3])/(run_layerdepth[3]*10*area.getValue()))*100);
+        }else{
+            actMPS4.setValue(0);
+            actLPS4.setValue(0);
+            actMoist4.setValue(0);}
+        
+        for (int i=0; i < nhor;i++){
+        run_actMoist_h[i] =  ((run_actMPS[i] + run_actLPS[i] + run_maxFPS[i])/(run_layerdepth[i]*10*area.getValue()))*100;
+        }
+        actMoist_h.setValue(run_actMoist_h);
+     int i = 0;
+    }
+    
+    public void cleanup() {
+        
+    }
+}
