bcffd830021e04a99a0fbcbf38753c59c9947bf4
Peter Krause <p.krause@uni-jena.de>
2008-12-04 06:57:39 +0000

    git-svn-id: http://svn.geogr.uni-jena.de/svn/jamsmodels/trunk@939 503a54e1-1403-0410-a754-803ee147b016

--- J2K/src/org/unijena/j2k/io/TSDataReader.java
+++ J2K/src/org/unijena/j2k/io/TSDataReader.java
@@ -106,7 +106,7 @@ public class TSDataReader extends JAMSComponent {
             update = JAMSVarDescription.UpdateType.INIT,
             description = "Calculate regression coefficients? If not, regCoeff array stays empty!"
             )
-            public JAMSInteger skipRegression;
+            public JAMSBoolean skipRegression;
     
     
     private JAMSTableDataStore store;
@@ -283,7 +283,7 @@ public class TSDataReader extends JAMSComponent {
     public void run() {
         
         dataArray.setValue(JAMSTableDataConverter.toDouble(store.getNext(), startColumn.getValue()));
-        if (skipRegression.getValue() != 1) {
+        if (!skipRegression.getValue()) {
             regCoeff.setValue(Regression.calcLinReg(elevation.getValue(), dataArray.getValue()));
         }
     }
