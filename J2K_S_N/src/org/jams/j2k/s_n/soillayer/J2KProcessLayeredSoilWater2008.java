c4585f2027e23112dbf53d54cad1322d700c6725
Manfred Fink <manfred.fink@uni-jena.de>
2009-09-15 11:24:32 +0000

    git-svn-id: http://svn.geogr.uni-jena.de/svn/jamsmodels/trunk@1050 503a54e1-1403-0410-a754-803ee147b016

--- J2K_S_N/src/org/jams/j2k/s_n/soillayer/J2KProcessLayeredSoilWater2008.java
+++ J2K_S_N/src/org/jams/j2k/s_n/soillayer/J2KProcessLayeredSoilWater2008.java
@@ -1361,7 +1361,9 @@ import jams.model.*;
    
 
         //Manfreds new
-        double potLPSoutflow = ((1 - (1 / (Math.pow(this.run_satHor[hor], 2) + alpha))) * this.run_actLPS[hor]) * (this.runkf_h[hor] / layerdepth.getValue()[hor]);
+        //double potLPSoutflow = ((1 - (1 / (Math.pow(this.run_satHor[hor], 2) + alpha))) * this.run_actLPS[hor]) * (this.runkf_h[hor] / layerdepth.getValue()[hor]);
+        //Peters second + kf and layerdepth
+        double potLPSoutflow = Math.pow(this.run_satHor[hor], alpha) * this.run_actLPS[hor] * (this.runkf_h[hor] / layerdepth.getValue()[hor]);
         //testing a simple function function out = 1/k * sto
         //double potLPSoutflow = 1 / alpha * this.act_LPS;//Math.pow(this.act_LPS, alpha);
         if(potLPSoutflow > this.run_actLPS[hor])
