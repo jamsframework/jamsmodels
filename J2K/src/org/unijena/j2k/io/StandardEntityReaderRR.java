570f9d8b0bd8556c4abbd1502075a0e567e7d1a3
Christin Michel <christin.michel@uni-jena.de>
2009-10-06 07:53:11 +0000

    git-svn-id: http://svn.geogr.uni-jena.de/svn/jamsmodels/trunk@1064 503a54e1-1403-0410-a754-803ee147b016

---
+++ J2K/src/org/unijena/j2k/io/StandardEntityReaderRR.java
@@ -0,0 +1,267 @@
+package org.unijena.j2k.io;
+
+import org.unijena.j2k.*;
+import jams.data.*;
+import jams.model.*;
+import java.util.*;
+import jams.JAMS;
+import jams.JAMSTools;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author S. Kralisch,C. Michel
+ */
+public class StandardEntityReaderRR extends JAMSComponent {
+    
+    @JAMSVarDescription(access = JAMSVarDescription.AccessType.READ, update = JAMSVarDescription.UpdateType.INIT, description = "HRU parameter file name")
+    public JAMSString hruFileName;
+    @JAMSVarDescription(access = JAMSVarDescription.AccessType.READ, update = JAMSVarDescription.UpdateType.INIT, description = "Reach parameter file name")
+    public JAMSString reachFileName;
+    @JAMSVarDescription(access = JAMSVarDescription.AccessType.WRITE, update = JAMSVarDescription.UpdateType.RUN, description = "Collection of hru objects")
+    public JAMSEntityCollection hrus;
+    @JAMSVarDescription(access = JAMSVarDescription.AccessType.WRITE, update = JAMSVarDescription.UpdateType.RUN, description = "Collection of reach objects")
+    public JAMSEntityCollection reaches;
+
+    public void init() throws JAMSEntity.NoSuchAttributeException {
+        //to handle relative path names        
+        hrus.setEntities(J2KFunctions.readParas(JAMSTools.CreateAbsoluteFileName(getModel().getWorkspaceDirectory().getPath(), hruFileName.getValue()), getModel()));
+
+        for (JAMSEntity e : hrus.getEntityArray()) {
+            try {
+                e.setId((long) e.getDouble("ID"));
+            } catch (JAMSEntity.NoSuchAttributeException nsae) {
+                getModel().getRuntime().sendErrorMsg("Couldn't find attribute \"ID\" while reading J2K HRU parameter file (" + hruFileName.getValue() + ")!");
+            }
+        }
+
+        //read reach parameter
+        //reaches = new JAMSEntityCollection();
+        reaches.setEntities(J2KFunctions.readParas(JAMSTools.CreateAbsoluteFileName(getModel().getWorkspaceDirectory().getPath(), reachFileName.getValue()), getModel()));
+
+        //create object associations from id attributes for hrus and reaches
+        createTopology();
+
+        //create total order on hrus and reaches that allows processing them subsequently
+        getModel().getRuntime().println("Create ordered hru-list", JAMS.VERBOSE);
+        createOrderedList(hrus, "to_poly");
+        getModel().getRuntime().println("Create ordered reach-list", JAMS.VERBOSE);
+        createOrderedList(reaches, "to_reach");
+        getModel().getRuntime().println("Entities read successfull!", JAMS.VERBOSE);
+        
+        //aendert die Ordnungsreihenfolge der Reaches
+        JAMSEntity[] Reaches= reaches.getEntityArray();
+        int laenge=Reaches.length;
+        JAMSEntity change;
+        for (int i=0;i<laenge/2;i++)
+        {
+          change=Reaches[i];
+          Reaches[i]=Reaches[laenge-1-i];
+          Reaches[laenge-1-i]=change;
+        }
+        ArrayList<JAMSEntity> list = new ArrayList<JAMSEntity>();
+        for (int i=0;i<laenge;i++)
+        {
+        list.add(Reaches[i]);
+        }
+
+        reaches.setEntities(list);
+        
+    }
+
+    //do depth first search to find cycles
+    protected boolean cycleCheck(JAMSEntity node, Stack<JAMSEntity> searchStack, HashSet<JAMSDouble> closedList, HashSet<JAMSDouble> visitedList) throws JAMSEntity.NoSuchAttributeException {
+        JAMSEntity child_node;
+
+        //current node allready in search stack -> circle found
+        if (searchStack.indexOf(node) != -1) {
+            int index = searchStack.indexOf(node);
+
+            String cyc_output = new String();
+            for (int i = index; i < searchStack.size(); i++) {
+                cyc_output += ((JAMSEntity) searchStack.get(i)).getDouble("ID") + " ";
+            }
+            getModel().getRuntime().println("Found circle with ids:" + cyc_output);
+
+            return true;
+        }
+        //node in closed list? -> then skip it
+        if (closedList.contains(node.getObject("ID")) == true) {
+            return false;
+        }
+        //now this node is visited
+        visitedList.add((JAMSDouble) node.getObject("ID"));
+
+        child_node = (JAMSEntity) node.getObject("to_poly");
+        if (child_node.getValue() == null) {
+            child_node = null;
+        }
+
+        if (child_node != null) {
+            //push current node to search stack
+            searchStack.push(node);
+
+            boolean result = cycleCheck(child_node, searchStack, closedList, visitedList);
+
+            searchStack.pop();
+
+            return result;
+        }
+        return false;
+    }
+
+    protected boolean cycleCheck() throws JAMSEntity.NoSuchAttributeException {
+        Iterator<JAMSEntity> hruIterator;
+
+        HashSet<JAMSDouble> closedList = new HashSet<JAMSDouble>();
+        HashSet<JAMSDouble> visitedList = new HashSet<JAMSDouble>();
+
+        JAMSEntity start_node;
+
+        getModel().getRuntime().println("Cycle checking...");
+
+        hruIterator = hrus.getEntities().iterator();
+
+        boolean result = false;
+
+        while (hruIterator.hasNext()) {
+            start_node = hruIterator.next();
+            //connected component of start_node allready processed?
+            if (closedList.contains(start_node.getObject("ID")) == false) {
+                if (cycleCheck(start_node, new Stack<JAMSEntity>(), closedList, visitedList) == true) {
+                    result = true;
+                }
+                closedList.addAll(visitedList);
+                visitedList.clear();
+            }
+
+        }
+        return result;
+    }
+
+    protected void createTopology() throws JAMSEntity.NoSuchAttributeException {
+
+        HashMap<Double, JAMSEntity> hruMap = new HashMap<Double, JAMSEntity>();
+        HashMap<Double, JAMSEntity> reachMap = new HashMap<Double, JAMSEntity>();
+        Iterator<JAMSEntity> hruIterator;
+        Iterator<JAMSEntity> reachIterator;
+        JAMSEntity e;
+
+        //put all entities into a HashMap with their ID as key
+        hruIterator = hrus.getEntities().iterator();
+        while (hruIterator.hasNext()) {
+            e = hruIterator.next();
+            hruMap.put(e.getDouble("ID"), e);
+        }
+        reachIterator = reaches.getEntities().iterator();
+        while (reachIterator.hasNext()) {
+            e = reachIterator.next();
+            reachMap.put(e.getDouble("ID"), e);
+        }
+
+        //create empty entities, i.e. those that are linked to in case there is no linkage ;-)
+        JAMSEntity nullEntity = JAMSDataFactory.createEntity();
+        nullEntity.setValue((HashMap<String, Object>) null);
+        hruMap.put(new Double(0), nullEntity);
+        reachMap.put(new Double(0), nullEntity);
+
+        //associate the hru entities with their downstream entity
+        hruIterator = hrus.getEntities().iterator();
+        while (hruIterator.hasNext()) {
+            e = hruIterator.next();
+            e.setObject("to_poly", hruMap.get(e.getDouble("to_poly")));
+            e.setObject("to_reach", reachMap.get(e.getDouble("to_reach")));
+        }
+
+        //associate the reach entities with their downstream entity
+        reachIterator = reaches.getEntities().iterator();
+        while (reachIterator.hasNext()) {
+            e = reachIterator.next();
+            e.setObject("to_reach", reachMap.get(e.getDouble("to-reach")));
+        }
+
+        //check for cycles
+        if (this.getModel().getRuntime().getDebugLevel() >= JAMS.VVERBOSE) {
+            if (cycleCheck() == true) {
+                getModel().getRuntime().println("HRUs --> cycle found ... :( ");
+            } else {
+                getModel().getRuntime().println("HRUs --> no cycle found");
+            }
+        }
+
+    }
+
+    protected void createOrderedList(JAMSEntityCollection col, String asso) throws JAMSEntity.NoSuchAttributeException {
+
+        Iterator<JAMSEntity> hruIterator;
+        JAMSEntity e, f;
+        ArrayList<JAMSEntity> newList = new ArrayList<JAMSEntity>();
+        HashMap<JAMSEntity, Integer> depthMap = new HashMap<JAMSEntity, Integer>();
+        Integer eDepth, fDepth;
+        boolean mapChanged = true;
+
+        hruIterator = col.getEntities().iterator();
+        while (hruIterator.hasNext()) {
+            depthMap.put(hruIterator.next(), new Integer(0));
+        }
+
+        int numHRUs = col.getEntities().size();
+
+        //put all collection elements (keys) and their depth (values) into a HashMap
+        int maxDepth = 0;
+        while (mapChanged) {
+            mapChanged = false;
+            hruIterator = col.getEntities().iterator();
+            while (hruIterator.hasNext()) {
+                e = hruIterator.next();
+
+                f = (JAMSEntity) e.getObject(asso);
+                if (f.getValue() == null) {
+                    f = null;
+                }
+
+                if (f != null) {
+                    eDepth = depthMap.get(e);
+                    fDepth = depthMap.get(f);
+                    if (fDepth.intValue() <= eDepth.intValue()) {
+                        depthMap.put(f, new Integer(eDepth.intValue() + 1));
+                        mapChanged = true;
+
+                    }
+                }
+            }
+        }
+
+        //find out which is the max depth of all entities
+        maxDepth = 0;
+        hruIterator = col.getEntities().iterator();
+        while (hruIterator.hasNext()) {
+            e = hruIterator.next();
+            maxDepth = Math.max(maxDepth, depthMap.get(e).intValue());
+        }
+
+        //create ArrayList of ArrayList objects, each element keeping the entities of one level
+        ArrayList<ArrayList<JAMSEntity>> alList = new ArrayList<ArrayList<JAMSEntity>>();
+        for (int i = 0; i <= maxDepth; i++) {
+            alList.add(new ArrayList<JAMSEntity>());
+        }
+
+        //fill the ArrayList objects within the ArrayList with entity objects
+        hruIterator = col.getEntities().iterator();
+        while (hruIterator.hasNext()) {
+            e = hruIterator.next();
+            int depth = depthMap.get(e).intValue();
+            alList.get(depth).add(e);
+        }
+
+        //put the entities
+        for (int i = 0; i <= maxDepth; i++) {
+            hruIterator = alList.get(i).iterator();
+            while (hruIterator.hasNext()) {
+                e = hruIterator.next();
+                newList.add(e);
+            }
+        }
+        col.setEntities(newList);
+    }
+}
---
+++ J2K/src/org/unijena/j2k/routing/CreateReachAttributeList.java
@@ -0,0 +1,112 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.unijena.j2k.routing;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.StringTokenizer;
+import jams.data.*;
+import jams.model.*;
+import java.util.ArrayList;
+import java.awt.Point;
+import javax.naming.directory.NoSuchAttributeException;
+/**
+ *
+ * @author sa63kul
+ */
+public class CreateReachAttributeList extends JAMSComponent {
+ 
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "The reach collection"
+            )
+            public JAMSEntityCollection entities;
+    
+ 
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "attribute slope"
+            )
+            public JAMSDouble slope;
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "attribute width"
+            )
+            public JAMSDouble width;
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "attribute roughness"
+            )
+            public JAMSDouble roughness;
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = ""
+            )
+            public JAMSEntity information;
+     @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READWRITE,
+            update = JAMSVarDescription.UpdateType.RUN,
+            description = "Reach statevar RG2 storage"
+            )
+            public JAMSDouble reachID; 
+      
+    
+    public void init()throws JAMSEntity.NoSuchAttributeException
+{
+
+}
+    
+    
+public void run()throws JAMSEntity.NoSuchAttributeException{
+ 
+// JAMSEntity entity = entities.getCurrent();
+ double reachID=this.reachID.getValue();
+ double width = this.width.getValue();
+ double slope = this.slope.getValue();
+ double rough = this.roughness.getValue();
+ 
+ ArrayList<ArrayList> actreachlist = new ArrayList<ArrayList>();
+        
+        try
+        {actreachlist=(ArrayList<ArrayList>)information.getObject("reachlist");}
+        catch (Exception e) {
+	    System.out.println(e.toString());
+	}
+  
+ double [][] actreachinfo =null; 
+ 
+        try
+        {actreachinfo=(double[][])information.getObject("reachinfo");}
+        catch (Exception e) {
+	    System.out.println(e.toString());
+	}
+ 
+actreachinfo[0][(int)reachID]=reachID;
+actreachinfo[1][(int)reachID]=slope; 
+actreachinfo[2][(int)reachID]=width;
+actreachinfo[3][(int)reachID]=rough;
+
+
+information.setObject("reachinfo", actreachinfo);
+
+}
+
+   
+public void cleanup() 
+{
+}
+}
