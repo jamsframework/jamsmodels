f004ce72516beb62e66a2d608879b46133769524
Heiko Busch <heiko.busch@uni-jena.de>
2010-02-10 12:23:51 +0000

    withInputHeader
    git-svn-id: http://svn.geogr.uni-jena.de/svn/jamsmodels/trunk@1092 503a54e1-1403-0410-a754-803ee147b016

--- J2K/src/org/unijena/j2k/io/StationDataWriter.java
+++ J2K/src/org/unijena/j2k/io/StationDataWriter.java
@@ -101,6 +101,17 @@ public class StationDataWriter extends JAMSComponent{
                         description = "Output file name")
     public JAMSString fileName;
 
+    /**
+     * this attribute controls, whether an input header is written or only 1 simple header-line
+     */
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            update = JAMSVarDescription.UpdateType.INIT,
+            description = "write file with input header",
+            defaultValue="true"
+            )
+            public JAMSString withInputHeader;
+
     private GenericDataWriter writer;
     private DateFormat dateFormat;
 
@@ -133,70 +144,68 @@ public class StationDataWriter extends JAMSComponent{
         }
         dateFormat.setTimeZone(JAMSCalendar.STANDARD_TIME_ZONE);
 
-        System.out.println("StationDataWriter. tres " + tempRes.getValue());
-        System.out.println("StationDataWriter. missDataValue " + missDataValue.getValue());
-
         writer = new GenericDataWriter(JAMSTools.CreateAbsoluteFileName(getModel().getWorkspace().getOutputDataDirectory().getPath(),fileName.getValue()));
-        //create and write a header first
+        String inputHeader = withInputHeader.getValue();
+        if (inputHeader != null && inputHeader.equalsIgnoreCase("true")) {
 
-        //write station meta data
-        writer.writeLine("#Calculated input data, generated: "+dt);
-        writer.writeLine(JAMSVarDescription.MD_DATAVALUEATTRIBS);
-        writer.writeLine(dataSetDesc.getValue());
-        writer.writeLine(JAMSVarDescription.MD_DATASETATTRIBS);
-        writer.writeLine(JAMSVarDescription.MD_MISSINGDATAVAL + SEPARATOR + missDataValue.getValue());
-        writer.writeLine(JAMSVarDescription.MD_DATASTART + SEPARATOR + timeInterval.getStart().toString(dateFormat));
-        writer.writeLine(JAMSVarDescription.MD_DATAEND + SEPARATOR + timeInterval.getEnd().toString(dateFormat));
-        writer.writeLine(JAMSVarDescription.MD_TEMP_RES + SEPARATOR + tempRes);
-        writer.writeLine(JAMSVarDescription.MD_STATATTRIBVAL);
-        writer.addColumn("name");
-        for (int i = 0; i < statNames.getValue().length; i++) {
-            writer.addColumn(statNames.getValue()[i]);
-        }
-        writer.writeHeader();
-        writer.write("ID");
-        for(int i = 0; i < statId.getValue().length; i++){
-            writer.write(SEPARATOR + statId.getValue()[i]);
-        }
-        writer.writeLine(EMPTY_CHAR);
-        writer.write("elevation");
-        for(int i = 0; i < statElev.getValue().length; i++){
-            writer.write(SEPARATOR + statElev.getValue()[i]);
-        }
-        writer.writeLine(EMPTY_CHAR);
-        writer.write("x");
-        for(int i = 0; i < statX.getValue().length; i++){
-            writer.write(SEPARATOR + statX.getValue()[i]);
-        }
-        writer.writeLine(EMPTY_CHAR);
-        writer.write("y");
-        for(int i = 0; i < statY.getValue().length; i++){
-            writer.write(SEPARATOR + statY.getValue()[i]);
-        }
-        writer.writeLine(EMPTY_CHAR);
-        writer.write("dataColumn");
-        for(int i = 0; i < statX.getValue().length; i++){
-            int col = i+1;
-            writer.write(SEPARATOR + col);
-        }
-        writer.writeLine(EMPTY_CHAR);
-        writer.writeLine(JAMSVarDescription.MD_DATAVAL);
+            //write station meta data as header
+            writer.writeLine("#Calculated input data, generated: "+dt);
+            writer.writeLine(JAMSVarDescription.MD_DATAVALUEATTRIBS);
+            writer.writeLine(dataSetDesc.getValue());
+            writer.writeLine(JAMSVarDescription.MD_DATASETATTRIBS);
+            writer.writeLine(JAMSVarDescription.MD_MISSINGDATAVAL + SEPARATOR + missDataValue.getValue());
+            writer.writeLine(JAMSVarDescription.MD_DATASTART + SEPARATOR + timeInterval.getStart().toString(dateFormat));
+            writer.writeLine(JAMSVarDescription.MD_DATAEND + SEPARATOR + timeInterval.getEnd().toString(dateFormat));
+            writer.writeLine(JAMSVarDescription.MD_TEMP_RES + SEPARATOR + tempRes);
+            writer.writeLine(JAMSVarDescription.MD_STATATTRIBVAL);
+            writer.addColumn("name");
+            for (int i = 0; i < statNames.getValue().length; i++) {
+                writer.addColumn(statNames.getValue()[i]);
+            }
+            writer.writeHeader();
+            writer.write("ID");
+            for(int i = 0; i < statId.getValue().length; i++){
+                writer.write(SEPARATOR + statId.getValue()[i]);
+            }
+            writer.writeLine(EMPTY_CHAR);
+            writer.write("elevation");
+            for(int i = 0; i < statElev.getValue().length; i++){
+                writer.write(SEPARATOR + statElev.getValue()[i]);
+            }
+            writer.writeLine(EMPTY_CHAR);
+            writer.write("x");
+            for(int i = 0; i < statX.getValue().length; i++){
+                writer.write(SEPARATOR + statX.getValue()[i]);
+            }
+            writer.writeLine(EMPTY_CHAR);
+            writer.write("y");
+            for(int i = 0; i < statY.getValue().length; i++){
+                writer.write(SEPARATOR + statY.getValue()[i]);
+            }
+            writer.writeLine(EMPTY_CHAR);
+            writer.write("dataColumn");
+            for(int i = 0; i < statX.getValue().length; i++){
+                int col = i+1;
+                writer.write(SEPARATOR + col);
+            }
+            writer.writeLine(EMPTY_CHAR);
+            writer.writeLine(JAMSVarDescription.MD_DATAVAL);
+        } else {
 
-/*
-        int cols = this.headers.getValue().length + 1;
-        String[] hdr = new String[cols];
-        hdr[0] = "date";
-        for(int i = 1; i < cols; i++)
-            hdr[i] = this.headers.getValue()[i-1];
-        this.headers.setValue(hdr);
+            //create and write a header from station names
+            int cols = this.statNames.getValue().length + 1;
+            String[] hdr = new String[cols];
+            hdr[0] = "date";
+            for(int i = 1; i < cols; i++)
+                hdr[i] = this.statNames.getValue()[i-1];
+            this.statNames.setValue(hdr);
 
-        for (int i = 0; i < headers.getValue().length; i++) {
-            writer.addColumn(headers.getValue()[i]);
+            for (int i = 0; i < statNames.getValue().length; i++) {
+                writer.addColumn(statNames.getValue()[i]);
+            }
+            writer.writeHeader();
         }
-        writer.writeHeader();
-*/
-
- }
+    }
 
     public void run() throws JAMSEntity.NoSuchAttributeException {
         writer.addData(time.toString(dateFormat));
