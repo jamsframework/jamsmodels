104920ab3c488e0a7b534b397e1478481faa1417
Sven Kralisch <sven@kralisch.com>
2013-03-07 11:36:08 +0000

    git-svn-id: http://svn.geogr.uni-jena.de/svn/jamsmodels/trunk@1313 503a54e1-1403-0410-a754-803ee147b016

--- J2K/src/org/unijena/j2k/io/StandardEntityReader.java
+++ J2K/src/org/unijena/j2k/io/StandardEntityReader.java
@@ -25,7 +25,7 @@ package org.unijena.j2k.io;
 //import org.unijena.j2k.*;
 import jams.JAMS;
 import jams.data.Attribute;
-import jams.data.JAMSDataFactory;
+import jams.data.DefaultDataFactory;
 import jams.model.JAMSComponent;
 import jams.model.JAMSComponentDescription;
 import jams.model.JAMSVarDescription;
