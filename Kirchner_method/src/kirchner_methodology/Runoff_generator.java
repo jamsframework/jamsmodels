e7a05862a7dd99b89157cb979d5d844ed5e8a353
Marko Adamovic <marko.adamovic@inrae.fr>
2013-09-04 08:30:19 +0000

    initial version of kirchner method components
    git-svn-id: http://svn.geogr.uni-jena.de/svn/jamsmodels/trunk@1533 503a54e1-1403-0410-a754-803ee147b016

---
+++ Kirchner_method/src/kirchner_methodology/Runoff_generator.java
@@ -0,0 +1,96 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kirchner_methodology;
+
+/**
+ *
+ * @author admin_adamovic
+ */
+/**
+ * JAMS example component - can be used as template for new components
+ */
+
+import jams.data.*;
+import jams.model.*;
+
+/**
+ *
+ * @author John Doe
+ */
+ @JAMSComponentDescription(
+    title="Title",
+    author="Author",
+    description="Description",
+    date = "YYYY-MM-DD",
+    version = "1.0_0"
+)
+@VersionComments(entries = {
+    @VersionComments.Entry(version = "1.0_0", comment = "Initial version"),
+    @VersionComments.Entry(version = "1.0_1", comment = "Some improvements")
+})        
+public class Runoff_generator extends JAMSComponent {
+
+    /*
+     *  Component attributes
+     */
+     
+     @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.WRITE          
+            )
+            public Attribute.Double discharge;                // for a list of attribute types, see jams.data.Attribute 
+     
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,       // type of access, i.e. READ, WRITE, READWRITE
+            description = "Description",                       // description of purpose
+            defaultValue = "0.0505",                                // default value, defaults to "%NULL%"
+            unit = "mm/h²",                                     // unit of this var if numeric, defaults to ""
+            lowerBound = 0,                                    // lowest allowed value of var if numeric, defaults to "0"
+            upperBound = 1000,                                 // highest allowed value of var if numeric, defaults to "0"
+            length = 0                                         // length of variable if string, defaults to "0"            
+            )
+            public Attribute.Double initialValue;   
+    
+    /*
+     *  Component run stages
+     */
+    // The number of steps to use in the interval
+  public static final int STEPS = 1000; 
+    @Override
+    public void init() {
+        
+        discharge.setValue(initialValue.getValue());
+        
+    }
+    
+public static void main(String[] argv)
+  {
+    // `h' is the size of each step.
+    double h = 1.0 ;
+    double s, s1, s2, s3, s4;
+    double x, y;
+    int i;
+    int dt=1;
+    double bypass_fraction=0.0074;
+    double [] qout=new double [];
+    // Computation by Euclid's method
+    // Initialize q
+    qout=discharge.get.Value;
+
+for (i=1; i<date.length; i++)
+    
+
+        s1 = qout(i)+dt/2*exp(c1+(c2-2)*qout(i)+c3*qout(i)*qout(i))*(((P(i)*(1-bypass_fraction))-(et_crop*potET(i)))-exp(qout(i)));
+        s2 = qout(i)+dt/2*exp(c1+(c2-2)*s1(i)+c3*s1(n,1)*s1(n,1))*(((P(i)*(1-bypass_fraction))-(et_crop*potET(i)))-exp(s1(i)));
+        s3= qout(i)+dt*exp(c1+(c2-2)*s2(i)+c3*s2(i)*s2(i))*(((P(i)*(1-bypass_fraction))-(et_crop*potET(i)))-exp(s2(i)));
+        s4= qout(i)+dt*exp(c1+(c2-2)*s3(i)+c3*s3(i)*s3(i))*(((P(i)*(1-bypass_fraction))-(et_crop*potET(i)))-exp(s3(i)));
+        s=(s1(i)/3)+(s2(i)*2/3)+(s3(i)/3)+(s4(i)/6)-qout(i)/2;
+
+        qout(i++)=s(i);
+       qoutsim(i)=((0.5*(exp(qout(i))+exp(qout(i++))))+(P(i)*(bypass_fraction)));
+     
+    }
+
+ 
