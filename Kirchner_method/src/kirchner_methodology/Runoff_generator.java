ef427c11ec481d0959e101d4de7b85e1c1afe2ac
Sven Kralisch <sven@kralisch.com>
2013-09-04 12:34:27 +0000

    git-svn-id: http://svn.geogr.uni-jena.de/svn/jamsmodels/trunk@1538 503a54e1-1403-0410-a754-803ee147b016

--- Kirchner_method/src/kirchner_methodology/Runoff_generator.java
+++ Kirchner_method/src/kirchner_methodology/Runoff_generator.java
@@ -11,7 +11,6 @@ package kirchner_methodology;
 /**
  * JAMS example component - can be used as template for new components
  */
-
 import jams.data.*;
 import jams.model.*;
 
@@ -19,78 +18,111 @@ import jams.model.*;
  *
  * @author John Doe
  */
- @JAMSComponentDescription(
-    title="Title",
-    author="Author",
-    description="Description",
-    date = "YYYY-MM-DD",
-    version = "1.0_0"
-)
+@JAMSComponentDescription(
+        title = "Title",
+        author = "Author",
+        description = "Description",
+        date = "YYYY-MM-DD",
+        version = "1.0_0")
 @VersionComments(entries = {
     @VersionComments.Entry(version = "1.0_0", comment = "Initial version"),
     @VersionComments.Entry(version = "1.0_1", comment = "Some improvements")
-})        
+})
 public class Runoff_generator extends JAMSComponent {
 
     /*
      *  Component attributes
      */
-     
-     @JAMSVarDescription(
-            access = JAMSVarDescription.AccessType.WRITE          
-            )
-            public Attribute.Double discharge;                // for a list of attribute types, see jams.data.Attribute 
-     
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "Description")
+    public Attribute.Double initialDischarge;
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "Description")
+    public Attribute.Double precip;
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "Description")
+    public Attribute.Double actET;
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "Description")
+    public Attribute.Double cropCoefficient;
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "Description")
+    public Attribute.Double c1;
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "Description")
+    public Attribute.Double c2;
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "Description")
+    public Attribute.Double c3;
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "Description",
+            defaultValue = "0.0074")
+    public Attribute.Double bypassFraction;
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.WRITE,
+            description = "Description")
+    public Attribute.Double bypass;
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READWRITE,
+            description = "Description")
+    public Attribute.Double discharge;
     
     @JAMSVarDescription(
-            access = JAMSVarDescription.AccessType.READ,       // type of access, i.e. READ, WRITE, READWRITE
-            description = "Description",                       // description of purpose
-            defaultValue = "0.0505",                                // default value, defaults to "%NULL%"
-            unit = "mm/h²",                                     // unit of this var if numeric, defaults to ""
-            lowerBound = 0,                                    // lowest allowed value of var if numeric, defaults to "0"
-            upperBound = 1000,                                 // highest allowed value of var if numeric, defaults to "0"
-            length = 0                                         // length of variable if string, defaults to "0"            
+            access = JAMSVarDescription.AccessType.READ, // type of access, i.e. READ, WRITE, READWRITE
+            description = "Description", // description of purpose
+            defaultValue = "0.0505", // default value, defaults to "%NULL%"
+            unit = "mm/h²", // unit of this var if numeric, defaults to ""
+            lowerBound = 0, // lowest allowed value of var if numeric, defaults to "0"
+            upperBound = 1000, // highest allowed value of var if numeric, defaults to "0"
+            length = 0 // length of variable if string, defaults to "0"            
             )
-            public Attribute.Double initialValue;   
-    
+    public Attribute.Double initialValue;
     /*
      *  Component run stages
      */
     // The number of steps to use in the interval
-  public static final int STEPS = 1000; 
+    public static final int STEPS = 1000;
+
     @Override
     public void init() {
-        
-        discharge.setValue(initialValue.getValue());
-        
-    }
-    
-public static void main(String[] argv)
-  {
-    // `h' is the size of each step.
-    double h = 1.0 ;
-    double s, s1, s2, s3, s4;
-    double x, y;
-    int i;
-    int dt=1;
-    double bypass_fraction=0.0074;
-    double [] qout=new double [];
-    // Computation by Euclid's method
-    // Initialize q
-    qout=discharge.get.Value;
-
-for (i=1; i<date.length; i++)
-    
-
-        s1 = qout(i)+dt/2*exp(c1+(c2-2)*qout(i)+c3*qout(i)*qout(i))*(((P(i)*(1-bypass_fraction))-(et_crop*potET(i)))-exp(qout(i)));
-        s2 = qout(i)+dt/2*exp(c1+(c2-2)*s1(i)+c3*s1(n,1)*s1(n,1))*(((P(i)*(1-bypass_fraction))-(et_crop*potET(i)))-exp(s1(i)));
-        s3= qout(i)+dt*exp(c1+(c2-2)*s2(i)+c3*s2(i)*s2(i))*(((P(i)*(1-bypass_fraction))-(et_crop*potET(i)))-exp(s2(i)));
-        s4= qout(i)+dt*exp(c1+(c2-2)*s3(i)+c3*s3(i)*s3(i))*(((P(i)*(1-bypass_fraction))-(et_crop*potET(i)))-exp(s3(i)));
-        s=(s1(i)/3)+(s2(i)*2/3)+(s3(i)/3)+(s4(i)/6)-qout(i)/2;
-
-        qout(i++)=s(i);
-       qoutsim(i)=((0.5*(exp(qout(i))+exp(qout(i++))))+(P(i)*(bypass_fraction)));
-     
+        discharge.setValue(initialDischarge.getValue());
     }
 
- 
+
+    @Override
+    public void run() {
+
+        double s, s1, s2, s3, s4;
+        int dt = 1;
+        double p = precip.getValue();
+        double qOld = discharge.getValue();
+        double qNew;
+        
+        
+        s1 = qOld + dt / 2 * Math.exp(c1.getValue() + (c2.getValue() - 2) * qOld + c3.getValue() * qOld * qOld) * 
+                 (((p * (1 - bypassFraction.getValue())) - (cropCoefficient.getValue() * actET.getValue())) - Math.exp(qOld));
+
+        s2 = qOld + dt / 2 * Math.exp(c1.getValue() + (c2.getValue() - 2) * s1 + c3.getValue() * s1 * s1) * 
+                 (((p * (1 - bypassFraction.getValue())) - (cropCoefficient.getValue() * actET.getValue())) - Math.exp(s1));
+
+        s3 = qOld + dt / 2 * Math.exp(c1.getValue() + (c2.getValue() - 2) * s2 + c3.getValue() * s2 * s2) * 
+                 (((p * (1 - bypassFraction.getValue())) - (cropCoefficient.getValue() * actET.getValue())) - Math.exp(s2));
+
+        s4 = qOld + dt / 2 * Math.exp(c1.getValue() + (c2.getValue() - 2) * s3 + c3.getValue() * s3 * s3) * 
+                 (((p * (1 - bypassFraction.getValue())) - (cropCoefficient.getValue() * actET.getValue())) - Math.exp(s3));
+        
+        s = (s1 / 3) + (s2 * 2 / 3) + (s3 / 3) + (s4 / 6) - qOld / 2;
+
+        qNew = ((0.5 * (Math.exp(qOld) + Math.exp(s))) + (p * (bypassFraction.getValue())));
+        
+        discharge.setValue(qNew);
+    }
+}
