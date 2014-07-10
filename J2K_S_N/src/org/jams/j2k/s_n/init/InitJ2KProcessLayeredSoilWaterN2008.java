2151232006a3216e03e65ad22940e7a8e1d31e49
Manfred Fink <manfred.fink@uni-jena.de>
2014-07-10 07:48:34 +0000

    git-svn-id: http://svn.geogr.uni-jena.de/svn/jamsmodels/trunk@1641 503a54e1-1403-0410-a754-803ee147b016

--- J2K_S_N/src/org/jams/j2k/s_n/init/InitJ2KProcessLayeredSoilWaterN2008.java
+++ J2K_S_N/src/org/jams/j2k/s_n/init/InitJ2KProcessLayeredSoilWaterN2008.java
@@ -153,17 +153,17 @@ public class InitJ2KProcessLayeredSoilWaterN2008 extends JAMSComponent {
             remRD = remRD - depth[h];
             }*/
 
-            acMPS[h] = 0;
+           
             mxMPS[h] = entity.getDouble(aNameFC + h) * area.getValue();
             mxFPS[h] = entity.getDouble(aNameDC + h) * area.getValue();
             mxLPS[h] = entity.getDouble(aNameAC + h) * area.getValue();
             corg[h] = entity.getDouble(corgName + h);
             root[h] = entity.getDouble(rootname + h);
-
+            acMPS[h] = mxMPS[h];
             bulk_density[h] = entity.getDouble(bulkdensityName + h);
             Kf_val[h] = entity.getDouble(KfName + h);
             acLPS[h] = 0;
-            stMPS[h] = 0;
+            stMPS[h] = 1;
             stLPS[h] = 0;
 
             inRD2[h] = 0;
