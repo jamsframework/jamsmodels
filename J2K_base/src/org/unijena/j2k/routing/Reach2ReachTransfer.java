6b790f3d8e3cd5f81fdeb2d9516924a6cef65153
Sven Kralisch <sven@kralisch.com>
2014-10-23 12:58:58 +0000

    git-svn-id: http://svn.geogr.uni-jena.de/svn/jamsmodels/trunk@1667 503a54e1-1403-0410-a754-803ee147b016

--- J2K_base/src/org/unijena/j2k/routing/Reach2ReachTransfer.java
+++ J2K_base/src/org/unijena/j2k/routing/Reach2ReachTransfer.java
@@ -19,7 +19,6 @@
  * along with JAMS. If not, see <http://www.gnu.org/licenses/>.
  *
  */
-
 package org.unijena.j2k.routing;
 
 import jams.data.*;
@@ -30,12 +29,12 @@ import jams.model.*;
  * @author Sven Kralisch
  */
 @JAMSComponentDescription(
-    title="Reach2ReachTransfer",
-    author="Sven Kralisch",
-    description="Simulation of artificial transfer of water and substances "
-            + "between reaches.",
-    date = "2014-10-23",
-    version = "1.0_0"
+        title = "Reach2ReachTransfer",
+        author = "Sven Kralisch",
+        description = "Simulation of artificial transfer of water and substances "
+        + "between reaches.",
+        date = "2014-10-23",
+        version = "1.0_0"
 )
 @VersionComments(entries = {
     @VersionComments.Entry(version = "1.0_0", comment = "Initial version")
@@ -45,62 +44,122 @@ public class Reach2ReachTransfer extends JAMSComponent {
     /*
      *  Component attributes
      */
-    
     @JAMSVarDescription(
             access = JAMSVarDescription.AccessType.READ,
-            description = "Target reach"
-            )
-            public Attribute.Entity target;
-    
-    @JAMSVarDescription(
-            access = JAMSVarDescription.AccessType.READWRITE,
-            description = "Components (water, substances) to be transferred"
-            )
-            public Attribute.Double[] values;
-    
-    @JAMSVarDescription(
-            access = JAMSVarDescription.AccessType.READ,    
-            description = "Target reach's receiving attributes"
-            )
-            public Attribute.String[] inNames;
+            description = "Reach collection"
+    )
+    public Attribute.EntityCollection reaches;
 
     @JAMSVarDescription(
-            access = JAMSVarDescription.AccessType.READ,    
-            description = "Fractions of components to be transferred"
-            )
-            public Attribute.Double[] fractions;
-    
+            access = JAMSVarDescription.AccessType.READ,
+            description = "Source reach ID"
+    )
+    public Attribute.Long sourceReachID;
+
     @JAMSVarDescription(
-            access = JAMSVarDescription.AccessType.READ,    
-            description = "Remove transferred volumes from source values?",
-            defaultValue= "false"
-            )
-            public Attribute.Boolean removeFromSource;    
-                
-    /*
-     *  Component run stages
-     */
-    
-    @Override
-    public void init() {
-    }
+            access = JAMSVarDescription.AccessType.READ,
+            description = "Target reach ID"
+    )
+    public Attribute.Long targetReachID;
+
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "Provisioning attributes of the source reach"
+    )
+    public Attribute.String[] sourceNames;
+
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "Receiving attributes of the target reach"
+    )
+    public Attribute.String[] targetNames;
+
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "Fraction of overall volume to be transferred",
+            defaultValue = "1"
+    )
+    public Attribute.Double fraction;
+
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "Lower boundary for transferred volume",
+            defaultValue = "-1"
+    )
+    public Attribute.Double lowerBound;
+
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "Upper boundary for transferred volume",
+            defaultValue = "-1"
+    )
+    public Attribute.Double upperBound;
+
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.WRITE,
+            description = "Component volumes that were transferred"
+    )
+    public Attribute.Double[] volumes;
 
     @Override
     public void run() {
-        
-        if(!target.getValue() .isEmpty()){
-            for (int i = 0; i < values.length; i++) {            
-                double x = values[i].getValue() * fraction[i].getValue();
-                target.setDouble(inNames[i].getValue(),target.getDouble(inNames[i].getValue()) + x);
-                if (removeFromSource.getValue()) {
-                    values[i].setValue(values[i].getValue() - x);
-                }
-            }   
-        }        
-        
+
+        // check if there is a source and receiver
+        Attribute.Entity source = reaches.getEntity(sourceReachID.getValue());
+        Attribute.Entity target = reaches.getEntity(targetReachID.getValue());
+        if (source == null || target == null || source.isEmpty() || target.isEmpty()) {
+            return;
+        }
+
+        // obtain the component values and calculate overall volume available 
+        // for transfer
+        double sum = 0;
+        double[] values = new double[sourceNames.length];
+        for (int i = 0; i < values.length; i++) {
+            values[i] = source.getDouble(sourceNames[i].getValue());
+            sum += values[i];
+        }
+
+        // calculate component fractions
+        double[] componentFractions = new double[values.length];
+        for (int i = 0; i < values.length; i++) {
+            componentFractions[i] = values[i] / sum;
+        }
+
+        // consider given fraction first
+        double targetVolume = sum * fraction.getValue();
+
+        // increase if targetVolume is lower than given lower bound
+        if (lowerBound.getValue() >= 0) {
+            targetVolume = Math.max(targetVolume, Math.min(sum, lowerBound.getValue()));
+        }
+
+        // decrease if targetVolume is lower than given lower bound
+        if (upperBound.getValue() >= 0) {
+            targetVolume = Math.min(targetVolume, upperBound.getValue());
+        }
+
+        // calculate fraction of overall volume that will be transferred
+        double fractionSum = targetVolume / sum;
+
+        // do the transfer
+        for (int i = 0; i < values.length; i++) {
+
+            // calculate volume to be removed from each component
+            double x = values[i] * componentFractions[i] * fractionSum;
+
+            // add this volume to the corresponding target component
+            target.setDouble(targetNames[i].getValue(), target.getDouble(targetNames[i].getValue()) + x);
+
+            // remove this volume from the source component
+            source.setDouble(sourceNames[i].getValue(), values[i] - x);
+
+            // store transferred volume in output attribute
+            if (volumes != null && volumes[i] != null) {
+                volumes[i].setValue(x);
+            }
+
+        }
     }
 
-    @Override
-    public void cleanup() {
-    }
 }
