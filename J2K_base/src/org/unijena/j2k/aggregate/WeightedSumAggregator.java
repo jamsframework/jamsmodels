1208ef1351e226081150a830f15d4c0cc915bad9
Sven Kralisch <sven@kralisch.com>
2016-05-31 12:09:55 +0000

    WeightedSumAggregator.java: added function to switch between multiplication and devision
    git-svn-id: http://svn.geogr.uni-jena.de/svn/jamsmodels/trunk@1820 503a54e1-1403-0410-a754-803ee147b016

--- J2K_base/src/org/unijena/j2k/aggregate/WeightedSumAggregator.java
+++ J2K_base/src/org/unijena/j2k/aggregate/WeightedSumAggregator.java
@@ -1,4 +1,5 @@
 package org.unijena.j2k.aggregate;
+
 /*
  * WeightedSumAggregator.java
  * Created on 22. Februar 2005, 15:01
@@ -30,46 +31,67 @@ import jams.data.*;
  * @author S. Kralisch
  */
 @JAMSComponentDescription(
-        title="WeightedSumAggregator",
-        author="Sven Kralisch",
-        description="Takes a list of double values as input and adds each "
+        title = "WeightedSumAggregator",
+        author = "Sven Kralisch",
+        version = "1.0",        
+        description = "Takes a list of double values as input and adds each "
         + "element to a corresponding element of an equally sized list of "
         + "output values. During aggrgation, the values are divided by weight. "
         + "Can be used to sum up any given number of double "
         + "values over a number of iteration steps."
-        )
+)
+@VersionComments(entries = {@VersionComments.Entry(
+        version = "1.1",
+        date = "2016-05-29",
+        comment = "Added option to multiply with weight instead of dividing only."
+)})
 public class WeightedSumAggregator extends JAMSComponent {
-    
+
     @JAMSVarDescription(
             access = JAMSVarDescription.AccessType.READ,
             description = "The value(s) to be summed up"
-            )
-            public Attribute.Double[] value;
-    
+    )
+    public Attribute.Double[] value;
+
     @JAMSVarDescription(
             access = JAMSVarDescription.AccessType.READ,
             description = "A weight that the value(s) are being devided by during aggregation",
             defaultValue = "1"
-            )
-            public Attribute.Double weight;
-    
+    )
+    public Attribute.Double weight;
+
     @JAMSVarDescription(
             access = JAMSVarDescription.AccessType.READWRITE,
             description = "The resulting weighted sum(s) of the given values"
-            )
-            public Attribute.Double[] sum;
-    
-    public void initAll(){
+    )
+    public Attribute.Double[] sum;
+
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "How should the weighting be done? If divideByWeight is true (default),"
+            + "the value will be divided by the weight, otherwise multiplied.",
+            defaultValue = "true"
+    )
+    public Attribute.Boolean divideByWeight;
+
+    public void initAll() {
         for (int i = 0; i < value.length; i++) {
             sum[i].setValue(0);
         }
     }
 
-    public void run() { 
-        for (int i = 0; i < value.length; i++) {
-            sum[i].setValue(sum[i].getValue()+ (value[i].getValue() / weight.getValue()));
-        }    
+    public void run() {
+
+        if (divideByWeight.getValue()) {
+            for (int i = 0; i < value.length; i++) {
+                sum[i].setValue(sum[i].getValue() + (value[i].getValue() / weight.getValue()));
+            }
+        } else {
+            for (int i = 0; i < value.length; i++) {
+                sum[i].setValue(sum[i].getValue() + (value[i].getValue() * weight.getValue()));
+            }
+        }
+
     }
-    
-    
+
 }
