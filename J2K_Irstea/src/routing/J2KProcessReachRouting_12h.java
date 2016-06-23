8b4d78c638b1224c2cf4973ad1c92115b4893b7a
Maxime Turko <maxime.turko@inrae.fr>
2016-06-23 12:13:37 +0000

    git-svn-id: http://svn.geogr.uni-jena.de/svn/jamsmodels/trunk@1824 503a54e1-1403-0410-a754-803ee147b016

--- J2K_Irstea/src/routing/J2KProcessReachRouting_12h.java
+++ J2K_Irstea/src/routing/J2KProcessReachRouting_12h.java
@@ -20,7 +20,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
  *
  */
-package org.unijena.j2k.routing;
+package routing;
 
 import jams.JAMS;
 import jams.data.*;
