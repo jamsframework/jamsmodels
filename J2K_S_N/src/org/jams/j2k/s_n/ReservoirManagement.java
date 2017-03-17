50584b2ea910b137bc678e28162910b1b8aa0762
Manfred Fink <manfred.fink@uni-jena.de>
2017-03-17 16:15:55 +0000

    first draft
    git-svn-id: http://svn.geogr.uni-jena.de/svn/jamsmodels/trunk@1843 503a54e1-1403-0410-a754-803ee147b016

--- J2K_S_N/src/org/jams/j2k/s_n/ReservoirManagement.java
+++ J2K_S_N/src/org/jams/j2k/s_n/ReservoirManagement.java
@@ -8,33 +8,193 @@ import jams.data.*;
 import jams.model.*;
 /**
  *
- * @author c6gohe2
+ * @author c8fima
  */
 @JAMSComponentDescription(
-        title="J2KPlantGrowthNitrogenStress",
+        title="ReservoirManagement",
         author="Manfred Fink",
-        description="Calculation of the plant groth nitrogen factor after SWAT"
+        description="Simple Reservoir module"
         )
         public class ReservoirManagement extends JAMSComponent {
 
-
     @JAMSVarDescription(
             access = JAMSVarDescription.AccessType.READ,
-            description = "HRU crop class"
+            description = "Desinged volume",
+            unit = "L"
+            )
+            public Attribute.Double desVolume;
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "inflow from reach",
+            unit = "L"
             )
             public Attribute.Double storageInput;
 
     @JAMSVarDescription(
             access = JAMSVarDescription.AccessType.READWRITE,
-            description = "HRU crop class"
+            description = "storage",
+            unit = "L"
             )
             public Attribute.Double storage;
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.WRITE,
+            description = "release from reservoir",
+            unit = "L"
+            )
+            public Attribute.Double outflow;
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.WRITE,
+            description = "actual fill proportion of the reservoir",
+            lowerBound = 0,
+            upperBound = 1
+            )
+            public Attribute.Double act_fillrate;
+    
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "proportion filled at the beginning",
+            lowerBound = 0,
+            upperBound = 1
+            )
+            public Attribute.Double initfilrate;
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "proportion were overproportional storage begin",
+            lowerBound = 0,
+            upperBound = 1
+            )
+            public Attribute.Double add_filrate;
+
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "proportion were overproportional release begin",
+            lowerBound = 0,
+            upperBound = 1
+            )
+            public Attribute.Double rele_filrate;
+    
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "optimal proportion",
+            lowerBound = 0,
+            upperBound = 1
+            )
+            public Attribute.Double opt_filrate;    
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "minimum outflow",
+            unit = "L",
+            lowerBound = 0,
+            upperBound = 1
+            )
+            public Attribute.Double min_flux;  
+
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "normal outflow",
+            unit = "L",
+            lowerBound = 0,
+            upperBound = 1
+            )
+            public Attribute.Double normal_flow;     
+    
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "floodprotection threshold",
+            unit = "L",
+            lowerBound = 0,
+            upperBound = 1
+            )
+            public Attribute.Double interception_inflow;  
+    
+    
 
 
 //Berechnung
-    public void run (){
+    public void init (){
+        
+        storage.setValue(desVolume.getValue() * initfilrate.getValue()); 
+        
+    }
     
-        storage.setValue(storageInput.getValue()*1000);
+    
+    
+    public void run (){
+        double pot_surplus = 0;
+        double run_storage = storage.getValue();
+        double run_storageInput = storageInput.getValue();
+        double run_min_flux = min_flux.getValue();
+        double run_add_filrate = add_filrate.getValue();
+        double run_rele_filrate = rele_filrate.getValue();
+        double run_normal_flow = normal_flow.getValue();
+        double run_interception_inflow = interception_inflow.getValue();
+        double run_demand = 0;
+        double pot_inter = 0;
+        double run_rele = 0;
+        double run_outflow = 0;
+        
+        
+        Double run_opt_filrate = opt_filrate.getValue();
+        
+        run_storage = run_storage + run_storageInput;
+                
+        double fillrate = storage.getValue()/desVolume.getValue();
+        
+        
+        if (fillrate < run_opt_filrate){
+            
+            pot_surplus = run_normal_flow - run_min_flux;
+            
+                      
+            run_demand = ((run_opt_filrate - fillrate)/(run_opt_filrate - run_add_filrate));
+            
+            run_outflow = Math.max(run_min_flux + (pot_surplus * run_demand), run_min_flux);
+            
+            
+        }else if (fillrate < 1){
+            
+            pot_inter =  run_interception_inflow - run_normal_flow;
+            
+            run_rele = ((fillrate - run_opt_filrate)/(run_rele_filrate - run_opt_filrate));
+            
+            run_outflow = Math.min(run_normal_flow + (pot_inter * run_rele), run_interception_inflow);     
+            
+            
+            
+        }else{
+            
+            run_outflow = run_storage - desVolume.getValue();
+            
+        
+        }
+        
+        
+        if (run_storage > run_outflow){
+            run_storage = run_storage - run_outflow;
+        }else{
+           run_outflow = run_storage;
+           run_storage  = 0;
+        }
+        
+        fillrate = run_storage/desVolume.getValue();
+        
+        act_fillrate.setValue(fillrate);
+        outflow.setValue(run_outflow);
+        storage.setValue(run_storage);
+        
+        
+        
+        
+        
 
     }
 
