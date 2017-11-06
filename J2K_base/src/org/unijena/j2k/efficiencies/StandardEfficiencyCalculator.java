73fa453f4a7358ac5c4f1a6b936ffa514f428fa3
Sven Kralisch <sven@kralisch.com>
2017-11-06 18:20:03 +0000

    Bugfix
    git-svn-id: http://svn.geogr.uni-jena.de/svn/jamsmodels/trunk@1854 503a54e1-1403-0410-a754-803ee147b016

--- J2K_base/src/org/unijena/j2k/efficiencies/StandardEfficiencyCalculator.java
+++ J2K_base/src/org/unijena/j2k/efficiencies/StandardEfficiencyCalculator.java
@@ -330,7 +330,7 @@ public class StandardEfficiencyCalculator extends JAMSComponent {
             this.interValEnd = this.interValStart + this.effTsteps;
         }
 
-        if (this.effMonthList != null && this.effMonthList.getValue().length > 0) {
+        if ((this.effMonthList != null) && (this.effMonthList.getValue().length > 0)) {
             this.monthly = true;
         }
     }
