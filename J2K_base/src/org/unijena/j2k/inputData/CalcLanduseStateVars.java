ef4e196ae8d215b3b8b609166b3a9fb8a70f612e
Manfred Fink <manfred.fink@uni-jena.de>
2019-02-01 15:50:00 +0000

    corrected elevation adaption switch
    git-svn-id: http://svn.geogr.uni-jena.de/svn/jamsmodels/trunk@1915 503a54e1-1403-0410-a754-803ee147b016

--- J2K_base/src/org/unijena/j2k/inputData/CalcLanduseStateVars.java
+++ J2K_base/src/org/unijena/j2k/inputData/CalcLanduseStateVars.java
@@ -35,12 +35,13 @@ import jams.model.*;
         description = "Calculates landuse state variables for a modelling unit"
         + "The calculation is done for a standard year (i.e. 366 days or 8784 hours)."
         + "The module can be used in hourly, daily and monthly resolution.",
-        version = "1.0_1",
-        date = "2016-05-13"
+        version = "1.0_2",
+        date = "2019-02-01"
 )
 @VersionComments(entries = {
     @VersionComments.Entry(version = "1.0_0", date = "2011-05-30", comment = "Initial version"),
-    @VersionComments.Entry(version = "1.0_1", date = "2016-05-13", comment = "Added default value for tempRes; added elevationAdaptation attribute to switch off adaptation of vegetation period depending on elevation")})
+    @VersionComments.Entry(version = "1.0_1", date = "2016-05-13", comment = "Added default value for tempRes; added elevationAdaptation attribute to switch off adaptation of vegetation period depending on elevation"),
+    @VersionComments.Entry(version = "1.0_2", date = "2019-02-01", comment = "Completed function of elevationAdaptation attribute affecting both effHeigth and LAI")})
 public class CalcLanduseStateVars extends JAMSComponent {
 
     /*
@@ -189,10 +190,18 @@ public class CalcLanduseStateVars extends JAMSComponent {
         //---------------------------------------
         // Calculation of Julian date of the specific points of LAI and eff. Height change
         //---------------------------------------
-        int d1 = (int) (d1_400 + 0.025 * (targetElevation - 400)); //start of spring
-        int d2 = (int) (d2_400 + 0.025 * (targetElevation - 400)); //start of summer
-        int d3 = (int) (d3_400 - 0.025 * (targetElevation - 400)); //start of autumn
-        int d4 = (int) (d4_400 - 0.025 * (targetElevation - 400)); //start of winter
+        int d1, d2, d3, d4;
+        if (elevationAdaptation.getValue()) {
+             d1 = (int) (d1_400 + 0.025 * (targetElevation - 400));
+             d2 = (int) (d2_400 + 0.025 * (targetElevation - 400));
+             d3 = (int) (d3_400 - 0.025 * (targetElevation - 400));
+             d4 = (int) (d4_400 - 0.025 * (targetElevation - 400));
+        } else {
+             d1 = (int) (d1_400);
+             d2 = (int) (d2_400);
+             d3 = (int) (d3_400);
+             d4 = (int) (d4_400);
+        }
 
         if ((lais[0] != lais[3]) && !LAIBugReported) {
             LAIBugReported = true;
