89131472c496b52dee01ff1881077ff211348ebd
Louise Crochemore <louise.crochemore@inrae.fr>
2021-06-02 11:25:59 +0000

    Dam initialisation based on the first date of the time interval (new argument read) Units for Smax and V0 set to Mm3
    git-svn-id: http://svn.geogr.uni-jena.de/svn/jamsmodels/trunk@1975 503a54e1-1403-0410-a754-803ee147b016

--- J2K_Irstea/src/management/DamDevice.java
+++ J2K_Irstea/src/management/DamDevice.java
@@ -47,151 +47,156 @@ public class DamDevice extends JAMSComponent {
     /*
      * Component variables
      */
-       @JAMSVarDescription (access = JAMSVarDescription.AccessType.READ,
-                         description = "regionalised data value (objective function)")
-    public Attribute.Double FO;
+        @JAMSVarDescription (
+            access = JAMSVarDescription.AccessType.READ,
+            description = "regionalised data value (objective function)"
+        )
+        public Attribute.Double FO;
         
-            @JAMSVarDescription(
+        @JAMSVarDescription(
             access = JAMSVarDescription.AccessType.READ,
             description = "Initial volume in the reservoir",
-            unit = "m3"
-            )
-            public Attribute.Double V0;
+            unit = "Mm3"
+        )
+        public Attribute.Double V0;
         
-                @JAMSVarDescription(
+        @JAMSVarDescription(
             access = JAMSVarDescription.AccessType.READ,
             description = "Maximum storage of the reservoir",
-            unit = "m3"
-            )
-            public Attribute.Double Smax;
+            unit = "Mm3"
+        )
+        public Attribute.Double Smax;
                 
-                  @JAMSVarDescription(
+        @JAMSVarDescription(
             access = JAMSVarDescription.AccessType.READWRITE,
             description = "RD1 inflow to reach",
             unit = "L"
-            )
-            public Attribute.Double inRD1;
+        )
+        public Attribute.Double inRD1;
     
-    @JAMSVarDescription(
+        @JAMSVarDescription(
             access = JAMSVarDescription.AccessType.READWRITE,
             description = "RD2 inflow to reach",
             unit = "L"
-            )
-            public Attribute.Double inRD2;
+        )
+        public Attribute.Double inRD2;
     
-    @JAMSVarDescription(
+        @JAMSVarDescription(
             access = JAMSVarDescription.AccessType.READWRITE,
             description = "RG1 inflow to reach",
             unit = "L"
-            )
-            public Attribute.Double inRG1;
+        )
+        public Attribute.Double inRG1;
     
-    @JAMSVarDescription(
+        @JAMSVarDescription(
             access = JAMSVarDescription.AccessType.READWRITE,
             description = "RG2 inflow to reach",
             unit = "L"
-            )
-            public Attribute.Double inRG2;
+        )
+        public Attribute.Double inRG2;
     
         @JAMSVarDescription(
             access = JAMSVarDescription.AccessType.READWRITE,
             description = "FO corrected if there isn't enough water in the river",
             unit = "L"
-            )
-            public Attribute.Double FO_fin;
-        
+        )
+        public Attribute.Double FO_fin;
         
         @JAMSVarDescription(
             access = JAMSVarDescription.AccessType.READWRITE,
             description = "state variable - Storage in the reservoir",
             unit = "L"
-            )
-            public Attribute.Double Storage;
+        )
+        public Attribute.Double Storage;
         
-            @JAMSVarDescription(access = JAMSVarDescription.AccessType.READ,
-    description = "Current time")
-    public Attribute.Calendar time;
+        @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "Current time"
+        )
+        public Attribute.Calendar time;
+            
+        @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "The time interval"
+        )
+        public Attribute.TimeInterval timeInterval;
     
 
     
-    
-  int nComp = 4;
+    int nComp = 4;
     double[] relComp;
     double[] runComp;
     double[] outComp;
     double currVolume = 0;
-    String Date2 = "1985-01-01 07:30";
     
     public void init() {
         relComp = new double[nComp];
         runComp = new double[nComp];
         outComp = new double[nComp];    
-        
     }
     
    
 
-   public void run() {
+    public void run() {
        
-       if (this.V0.getValue() > 0) {
-           
-        String Date = time.toString();
-        if ( (Date.equals(Date2) )) {
-            this.Storage.setValue(Math.pow(10,9)*this.V0.getValue());
-        }
-        
-        double test = this.inRD1.getValue();
-        test = test + this.inRD2.getValue();
-        test = test + this.inRG1.getValue();
-        test = test + this.inRG2.getValue();
-        double runOutflow = 0;
-        double newS = 0;
-        double FO_act = 0;
-        
-       calcRelComponents(); 
-        
-        this.runComp[0] = this.inRD1.getValue();
-        this.runComp[1] = this.inRD2.getValue();
-        this.runComp[2] = this.inRG1.getValue();
-        this.runComp[3] = this.inRG2.getValue();
-        
-        calcRelComponents();
-    FO_act = this.FO.getValue();
-    if(FO_act >= 0){
-        //Cas de restitution
-        if (this.Storage.getValue() < FO_act) {FO_act = this.Storage.getValue();}
-        newS = Math.max(this.Storage.getValue() - FO_act,0);
-}    else  {
-        //Cas de stockage
-        // in case test < FO, we put FO = test 
-        // because we can't keep more water than there is in the river
-        
-        if( (test+ FO_act) <0) { FO_act = -test;} 
-        
-        newS = Math.min(this.Storage.getValue() - FO_act,Math.pow(10,9)*this.Smax.getValue());        
-        
-}
-  
-    // Calcul de la restitution réelle
-    runOutflow = Math.max(0,test -(newS- this.Storage.getValue()));
-    this.Storage.setValue(newS);   
-    this.FO_fin.setValue(FO_act) ; 
-       for(int i = 0; i < runComp.length; i++){
+        if (this.V0.getValue() > 0) {
+
+            String Date = time.toString();
+            String Start = timeInterval.getStart().toString();
+            if ( (Date.equals(Start) )) {
+                this.Storage.setValue(Math.pow(10,9)*this.V0.getValue());
+            }
+
+            double test = this.inRD1.getValue();
+            test = test + this.inRD2.getValue();
+            test = test + this.inRG1.getValue();
+            test = test + this.inRG2.getValue();
+            double runOutflow = 0;
+            double newS = 0;
+            double FO_act = 0;
+
+            calcRelComponents(); 
+
+            this.runComp[0] = this.inRD1.getValue();
+            this.runComp[1] = this.inRD2.getValue();
+            this.runComp[2] = this.inRG1.getValue();
+            this.runComp[3] = this.inRG2.getValue();
+
+            calcRelComponents();
+            
+            FO_act = this.FO.getValue();
+            if(FO_act >= 0){
+                //Cas de restitution
+                if (this.Storage.getValue() < FO_act) {FO_act = this.Storage.getValue();}
+                newS = Math.max(this.Storage.getValue() - FO_act,0);
+            }    else  {
+                //Cas de stockage
+                // in case test < FO, we put FO = test 
+                // because we can't keep more water than there is in the river
+                if( (test+ FO_act) <0) { FO_act = -test;} 
+                newS = Math.min(this.Storage.getValue() - FO_act,Math.pow(10,9)*this.Smax.getValue());        
+            }
+
+            // Calcul de la restitution réelle
+            runOutflow = Math.max(0,test -(newS- this.Storage.getValue()));
+            this.Storage.setValue(newS);   
+            this.FO_fin.setValue(FO_act) ; 
+            for(int i = 0; i < runComp.length; i++){
                 outComp[i] = runOutflow * relComp[i];
                 runComp[i] = runComp[i] - outComp[i];
             }
-    this.inRD1.setValue(outComp[0]);  
-    this.inRD2.setValue(outComp[1]);  
-    this.inRG1.setValue(outComp[2]);  
-    this.inRG2.setValue(outComp[3]);  
-       
-       }    else {
-              this.Storage.setValue(0.0);
-              this.FO_fin.setValue(0.0) ; 
-       }
-   }
+            this.inRD1.setValue(outComp[0]);  
+            this.inRD2.setValue(outComp[1]);  
+            this.inRG1.setValue(outComp[2]);  
+            this.inRG2.setValue(outComp[3]);  
+
+        }    else {
+            this.Storage.setValue(0.0);
+            this.FO_fin.setValue(0.0) ; 
+        }
+    }
     
-      private void calcRelComponents(){
+    private void calcRelComponents(){
         currVolume = 0;
         for(int i = 0; i < nComp; i++){
             currVolume = currVolume + runComp[i];
