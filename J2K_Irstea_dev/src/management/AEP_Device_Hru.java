0a2f19145fafc2c545da33c9284cdcf9fcb65bdd
Louise Crochemore <louise.crochemore@inrae.fr>
2021-06-16 15:56:28 +0000

    Updated to take only water from groundwater storages (RG)
    git-svn-id: http://svn.geogr.uni-jena.de/svn/jamsmodels/trunk@1987 503a54e1-1403-0410-a754-803ee147b016

--- J2K_Irstea_dev/src/management/AEP_Device_Hru.java
+++ J2K_Irstea_dev/src/management/AEP_Device_Hru.java
@@ -53,13 +53,6 @@ public class AEP_Device_Hru extends JAMSComponent {
             description = "regionalised data value (objective function)"
         )
         public Attribute.Double FO;
-                
-        @JAMSVarDescription(
-            access = JAMSVarDescription.AccessType.READWRITE,
-            description = "RD1 inflow to HRU",
-            unit = "L"
-        )
-        public Attribute.Double inRD1;
     
         @JAMSVarDescription(
             access = JAMSVarDescription.AccessType.READWRITE,
@@ -67,7 +60,7 @@ public class AEP_Device_Hru extends JAMSComponent {
             unit = "L"
         )
         public Attribute.Double inRD2;
-    
+        
         @JAMSVarDescription(
             access = JAMSVarDescription.AccessType.READWRITE,
             description = "RG1 inflow to HRU",
@@ -116,74 +109,79 @@ public class AEP_Device_Hru extends JAMSComponent {
 
         public void run() {
        
-            // Total inflow
-            double totalIn = this.inRD1.getValue() + this.inRD2.getValue() + this.inRG1.getValue() + this.inRG2.getValue();
-            
-            // Water already in the reach available for extraction 
-            // -- see if actPrel relevant in management scenarios
-            // -- set to 1 at first because AEP is generally prioritized over other uses
-            double totalAct = this.actPrel.getValue() * (actRG1.getValue() + actRG2.getValue()); // eau du reach dispo pour l'irrigation.
-        
-            // Water to extract or release
-            double FO_act = this.FO.getValue();
-            
-            
-            //Case of extraction
-            if (FO_act < 0) {
+            // Check if HRU related to AEP transfers
+            if (!hrus.getCurrent().existsAttribute("AEP")) {
                 
-                // Account for losses in the network
-                FO_act = FO_act + netLoss.getValue()*FO_act;
-                //looking if we can cover the demand with in
-                double frac = FO_act/totalIn;
-                
-                if (frac >= -1) {
+                // Total inflow
+                double totalIn = this.inRG1.getValue() + this.inRG2.getValue();
 
-                    //we can cover all only with in to the reach, reduce the components accordingly
-                    inRD1.setValue(inRD1.getValue() * (1 - frac));
-                    inRD2.setValue(inRD2.getValue() * (1 - frac));
-                    inRG1.setValue(inRG1.getValue() * (1 - frac));
-                    inRG2.setValue(inRG2.getValue() * (1 - frac));
-                    this.FO_fin.setValue(FO_act) ;
+                // Water already in the reach available for extraction 
+                // -- see if actPrel relevant in management scenarios
+                // -- set to 1 at first because AEP is generally prioritized over other uses
+                double totalAct = this.actPrel.getValue() * (actRG1.getValue() + actRG2.getValue()); // eau du reach dispo pour l'irrigation.
 
-                } else {
-                    //looking if we can cover the demand by including part of act...
-                    frac = FO_act / (totalIn+totalAct);
+                // Water to extract or release
+                double FO_act = this.FO.getValue();
 
-                    //we can cover only part of the demand with in, reduce the components to 0
-                    inRD1.setValue(0);
-                    inRD2.setValue(0);
-                    inRG1.setValue(0);
-                    inRG2.setValue(0);
+
+                //Case of extraction
+                if (FO_act < 0) {
+
+                    // Account for losses in the network
+                    FO_act = FO_act + netLoss.getValue()*FO_act;
+                    //looking if we can cover the demand with in
+                    double frac = FO_act/totalIn;
 
                     if (frac >= -1) {
-                        //we can cover all of the demand but not only with in..., reduce the components accordingly
-                        double actDemand = 0;
-                        actDemand = FO_act + totalIn;
-                        double frac2 = -actDemand/totalAct;
-                        if(frac2<0) {
-                            getModel().getRuntime().println("Warning: error in sign when extracting drinking water in reach");
-                        }
-                        actRG1.setValue(actRG1.getValue() * (1 - frac2));
-                        actRG2.setValue(actRG2.getValue() * (1 - frac2));
+
+                        //we can cover all only with in to the reach, reduce the components accordingly
+                        inRG1.setValue(inRG1.getValue() * (1 - frac));
+                        inRG2.setValue(inRG2.getValue() * (1 - frac));
                         this.FO_fin.setValue(FO_act) ;
 
                     } else {
-                        // we can cover part of the demand ; reduce the act... to (1 - actPrel)*act...
-                        actRG1.setValue(actRG1.getValue() * (1 - actPrel.getValue()));
-                        actRG2.setValue(actRG2.getValue() * (1 - actPrel.getValue()));
-                        this.FO_fin.setValue(-(totalIn+totalAct));
+                        //looking if we can cover the demand by including part of act...
+                        frac = FO_act / (totalIn+totalAct);
+
+                        //we can cover only part of the demand with in, reduce the components to 0
+                        inRG1.setValue(0);
+                        inRG2.setValue(0);
+
+                        if (frac >= -1) {
+                            //we can cover all of the demand but not only with in..., reduce the components accordingly
+                            double actDemand = 0;
+                            actDemand = FO_act + totalIn;
+                            double frac2 = -actDemand/totalAct;
+                            if(frac2<0) {
+                                getModel().getRuntime().println("Warning: error in sign when extracting drinking water in reach");
+                            }
+                            actRG1.setValue(actRG1.getValue() * (1 - frac2));
+                            actRG2.setValue(actRG2.getValue() * (1 - frac2));
+                            this.FO_fin.setValue(FO_act) ;
+
+                        } else {
+                            // we can cover part of the demand ; reduce the act... to (1 - actPrel)*act...
+                            actRG1.setValue(actRG1.getValue() * (1 - actPrel.getValue()));
+                            actRG2.setValue(actRG2.getValue() * (1 - actPrel.getValue()));
+                            this.FO_fin.setValue(-(totalIn+totalAct));
+                        }
                     }
+
+                    // restitute lost water to RD2 (when efficiency of the network netLoss <1) :
+                    inRD2.setValue(inRD2.getValue()+Math.max(0.,-netLoss.getValue()*this.FO_fin.getValue()));
+
+                //Case of release - not allowed in HRU
+                } else {
+
+                    getModel().getRuntime().println("Error: Drinking water restitution in HRU:"+hrus.getCurrent().getId());
+                    System.exit(1);
+
                 }
                 
-                // restitute lost water to RD2 (when efficiency of the network netLoss <1) :
-                inRD2.setValue(inRD2.getValue()+Math.max(0.,-netLoss.getValue()*this.FO_fin.getValue()));
-            
-            //Case of release - not allowed in HRU
             } else {
                 
-                getModel().getRuntime().println("Error: Drinking water restitution in HRU:"+hrus.getCurrent().getId());
-                return;
-
+                this.FO_fin.setValue(0.0);
+            
             }
         }
 
