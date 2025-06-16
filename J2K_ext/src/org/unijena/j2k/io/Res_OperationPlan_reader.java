8e6b3e915970bd9155570b8b2bf9fe80a1e4f3a5
Manfred Fink <manfred.fink@uni-jena.de>
2025-06-16 21:22:45 +0000

    git-svn-id: http://svn.geogr.uni-jena.de/svn/jamsmodels/trunk@2174 503a54e1-1403-0410-a754-803ee147b016

---
+++ J2K_ext/src/org/unijena/j2k/io/Res_OperationPlan_reader.java
@@ -0,0 +1,209 @@
+/*
+ * StandardLUReader.java
+ * Created on 10. November 2005, 10:53
+ *
+ * This file is part of JAMS
+ * Copyright (C) 2005 S. Kralisch and P. Krause
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ *
+ */
+package org.unijena.j2k.io;
+
+import org.unijena.j2k.*;
+import jams.data.*;
+import jams.model.*;
+import java.util.*;
+import jams.JAMS;
+import jams.tools.FileTools;
+
+/**
+ *
+ * @author M. Fink
+ */
+@JAMSComponentDescription(title = "Res_OperationPlan_reader",
+        author = "Manfred Fink",
+        description = "This component reads an ASCII file containing Pool-based "
+        + "Operating Plan information and adds them to model entities.",
+        date = "2025-05-20",
+        version = "1.0_0")
+public class Res_OperationPlan_reader extends JAMSComponent {
+
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "Pool-based Operating Plan parameter file name"
+    )
+    public Attribute.String LamelleName;
+
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "List of Reach objects"
+    )
+    public Attribute.EntityCollection reaches;
+
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "ID of reach where the reservoir is located"
+    )
+    public Attribute.Double RID;
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "initial storage in the reservoir [m³]",
+            unit = "m³",
+            defaultValue = "0.0")
+    public Attribute.Double res_init;
+    
+           @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "Initial RD1 storage proportion in the reservoir",
+            unit = "-",
+            defaultValue = "0.25"
+    )
+    public Attribute.Double res_init_porp_RD1;
+
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "Initial RD2 storage proportion in the reservoir",
+            unit = "-",
+            defaultValue = "0.25"
+    )
+    public Attribute.Double res_init_porp_RD2;
+    
+        @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "Initial RG1 storage proportion in the reservoir",
+            unit = "-",
+            defaultValue = "0.25"
+    )
+    public Attribute.Double res_init_porp_RG1;
+        
+        @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "Initial RG2 storage proportion in the reservoir",
+            unit = "-",
+            defaultValue = "0.25"
+    )
+    public Attribute.Double res_init_porp_RG2;
+            
+   @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.WRITE,
+            description = "initial storage in the reservoir [l]",
+            unit = "l",
+            defaultValue = "0.0")
+    public Attribute.Double res_init_l;        
+   
+       @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.WRITE,
+            description = "RD1 storage in the reservoir",
+            unit = "L"
+    )
+    public Attribute.Double res_init_RD1;
+
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.WRITE,
+            description = "RD2 storage in the reservoir",
+            unit = "L"
+    )
+    public Attribute.Double res_init_RD2;
+    
+        @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.WRITE,
+            description = "RG1 storage in the reservoir",
+            unit = "L"
+    )
+    public Attribute.Double res_init_RG1;
+        
+        @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.WRITE,
+            description = "RG2 storage in the reservoir",
+            unit = "L"
+    )
+    public Attribute.Double res_init_RG2;
+
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.WRITE,
+            description = "Number of Pools"
+    )
+    public Attribute.Integer numPools;
+
+    public void init() {
+        //read lu parameter
+        Attribute.EntityCollection PBOPS = getModel().getRuntime().getDataFactory().createEntityCollection();
+
+        PBOPS.setEntities(J2KFunctions.readParas(FileTools.createAbsoluteFileName(getModel().getWorkspaceDirectory().getPath(), LamelleName.getValue()), getModel()));
+
+        HashMap<Double, Attribute.Entity> PBOPSMap = new HashMap<Double, Attribute.Entity>();
+        Attribute.Entity PBOP, e;
+        Object[] attrs;
+
+        //put all entities into a HashMap with their ID as key
+        Iterator<Attribute.Entity> luIterator = PBOPS.getEntities().iterator();
+        while (luIterator.hasNext()) {
+            PBOP = luIterator.next();
+
+            PBOPSMap.put(PBOP.getDouble("Pool"), PBOP);
+        }
+
+        Iterator<Attribute.Entity> reachIterator = reaches.getEntities().iterator();
+        int nrPools = 0;
+        while (reachIterator.hasNext()) {
+            e = reachIterator.next();
+
+            if (e.getDouble("ID") == RID.getValue()) {
+
+                nrPools = PBOPSMap.size();
+
+                int i = 0;
+                while (i < nrPools) {
+                    i = i + 1;
+                    double j = i;
+                    PBOP = PBOPSMap.get(j);
+                    attrs = PBOP.getKeys();
+
+                    for (int k = 0; k < attrs.length; k++) {
+                        //e.setDouble((String) attrs[i], lu.getDouble((String) attrs[i]));
+                        Object o = PBOP.getObject((String) attrs[k]);
+
+                        e.setObject((String) attrs[k] + i, o);
+
+                    }
+
+
+                    e.setObject("attributes", attrs);
+
+                    //Object nrPoolso = nrPools;
+                    //e.setObject((String) "nrPools", nrPoolso);
+                    e.setInt("nrPools", nrPools);
+                }
+                double res_storage = res_init.getValue()*1000;
+                numPools.setValue(nrPools);
+                res_init_l.setValue(res_storage);
+                
+                double propsum  = res_init_porp_RD1.getValue() + res_init_porp_RD2.getValue() + res_init_porp_RG1.getValue() +res_init_porp_RG2.getValue();
+                
+                res_init_RD1.setValue((res_storage * res_init_porp_RD1.getValue()) /propsum);
+                res_init_RD2.setValue((res_storage * res_init_porp_RD2.getValue()) /propsum);
+                res_init_RG1.setValue((res_storage * res_init_porp_RG1.getValue()) /propsum);
+                res_init_RG2.setValue((res_storage * res_init_porp_RG2.getValue()) /propsum);
+                
+                int d = 0;
+
+            }
+        }
+        getModel().getRuntime().println("Pool-based Operating Plan parameter file processed ...", JAMS.VERBOSE);
+    }
+
+}
---
+++ J2K_ext/src/org/unijena/j2k/routing/ReservoirMan_plan.java
@@ -0,0 +1,252 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.unijena.j2k.routing;
+
+import jams.data.*;
+import jams.model.*;
+import java.util.Iterator;
+import org.unijena.j2k.routing.J2KProcessReachRouting;
+
+/**
+ *
+ * @author c8fima
+ */
+@JAMSComponentDescription(
+        title = "Daily Reservoir module",
+        author = "Manfred Fink",
+        description = "Reservoir Management according to pool based management ;"
+        + "reservoir plan for daily timesteps; no proper flood mode"
+        + "integration into the reaches",
+        date = "21.05.2025",
+        version = "1.0"
+)
+public class ReservoirMan_plan extends J2KProcessReachRouting {
+
+
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "time")
+    public Attribute.Calendar time;
+
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.WRITE,
+            description = "outflow from reservoir",
+            unit = "L"
+    )
+    public Attribute.Double Simrunoff;
+
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.WRITE,
+            description = "initial storage in the reservoir [l]",
+            unit = "L",
+            defaultValue = "0.0")
+    public Attribute.Double res_init_l;
+
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READWRITE,
+            description = "storage in the reservoir",
+            unit = "L"
+    )
+    public Attribute.Double res_storage;
+
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READWRITE,
+            description = "RD1 storage in the reservoir",
+            unit = "L"
+    )
+    public Attribute.Double res_storage_RD1;
+
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READWRITE,
+            description = "RD2 storage in the reservoir",
+            unit = "L"
+    )
+    public Attribute.Double res_storage_RD2;
+
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READWRITE,
+            description = "RG1 storage in the reservoir",
+            unit = "L"
+    )
+    public Attribute.Double res_storage_RG1;
+
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READWRITE,
+            description = "RG2 storage in the reservoir",
+            unit = "L"
+    )
+    public Attribute.Double res_storage_RG2;
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "RD1 storage in the reservoir",
+            unit = "L"
+    )
+    public Attribute.Double res_init_RD1;
+
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "RD2 storage in the reservoir",
+            unit = "L"
+    )
+    public Attribute.Double res_init_RD2;
+    
+        @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "RG1 storage in the reservoir",
+            unit = "L"
+    )
+    public Attribute.Double res_init_RG1;
+        
+        @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "RG2 storage in the reservoir",
+            unit = "L"
+    )
+    public Attribute.Double res_init_RG2;
+
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.WRITE,
+            description = "release from reservoir",
+            unit = "L"
+    )
+    public Attribute.Double outflow;
+
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "Number of Pools"
+    )
+    public Attribute.Integer numPools;
+
+//Berechnung
+    public void initAll() {
+        Attribute.Entity reach;
+
+        Iterator<Attribute.Entity> reachIterator = entities.getEntities().iterator();
+
+        while (reachIterator.hasNext()) {
+            reach = reachIterator.next();
+            if (reach.existsAttribute("nrPools")) {
+                res_storage.setValue(res_init_l.getValue());
+                res_storage_RD1.setValue(res_init_RD1.getValue());
+                res_storage_RD2.setValue(res_init_RD2.getValue());
+                res_storage_RG1.setValue(res_init_RG1.getValue());
+                res_storage_RG2.setValue(res_init_RG2.getValue());
+            }
+        }
+
+    }
+
+    public void run() {
+
+        Attribute.Entity reach;
+
+        Iterator<Attribute.Entity> reachIterator = entities.getEntities().iterator();
+
+        while (reachIterator.hasNext()) {
+            reach = reachIterator.next();
+            if (reach.existsAttribute("nrPools")) {
+                double pot_surplus = 0;
+                double run_res_storage = res_storage.getValue();
+
+                double run_demand = 0;
+                double pot_inter = 0;
+                double run_rele = 0;
+                double run_outflow = 0;
+                int month = time.get(Attribute.Calendar.MONTH) + 1;
+                int nrPools = numPools.getValue();
+                int actPoolnr = 0;
+
+                double reservoir_V = res_storage.getValue();
+
+
+                double RD1act = actRD1.getValue() + inRD1.getValue();
+                double RD2act = actRD2.getValue() + inRD2.getValue();
+                double RG1act = actRG1.getValue() + inRG1.getValue();
+                double RG2act = actRG2.getValue() + inRG2.getValue();
+
+                //double addInAct = actAddIn.getValue() + this.inAddIn.getValue();
+
+                inRD1.setValue(0);
+                inRD2.setValue(0);
+                inRG1.setValue(0);
+                inRG2.setValue(0);
+
+                //inAddIn.setValue(0);
+
+                actRD1.setValue(0);
+                actRD2.setValue(0);
+                actRG1.setValue(0);
+                actRG2.setValue(0);
+
+                //actAddIn.setValue(0);
+
+                double run_res_storage_RD1 = res_storage_RD1.getValue() + RD1act;
+                double run_res_storage_RD2 = res_storage_RD2.getValue() + RD2act;
+                double run_res_storage_RG1 = res_storage_RG1.getValue() + RG1act;
+                double run_res_storage_RG2 = res_storage_RG2.getValue() + RG2act;
+
+                double run_res_storageInput = (RD1act + RD2act + RG1act + RG2act);
+                run_res_storage = run_res_storage + run_res_storageInput;
+
+                double run_res_prop_RD1 = run_res_storage_RD1 / run_res_storage;
+                double run_res_prop_RD2 = run_res_storage_RD2 / run_res_storage;
+                double run_res_prop_RG1 = run_res_storage_RG1 / run_res_storage;
+                double run_res_prop_RG2 = run_res_storage_RG2 / run_res_storage;
+
+                //Calculation of poolbased outflow
+                int i = 0;
+                i = 0;
+                while (i < nrPools) {
+                    i++;
+                    getModel().getRuntime().sendErrorMsg("I Variable = " + i + "!");
+                    if (reach.getDouble("V_begin" + i) * 1000 <= reservoir_V && reach.getDouble("V_end" + i) * 1000 > reservoir_V) {
+                        run_outflow = 86400 * reach.getDouble("Q" + month + i);
+                        run_outflow = Math.min(run_outflow, run_res_storage);
+                    } else if (reach.getDouble("V_begin" + (i + 1)) * 1000 <= reservoir_V && reach.getDouble("V_end" + i) * 1000 > reservoir_V) { // gap between pools
+                        run_outflow = 86400 * reach.getDouble("Q" + month + i);
+                        run_outflow = Math.min(run_outflow, run_res_storage);
+                    } else if (reservoir_V > reach.getDouble("V_end" + nrPools) * 1000) {  //stage over capacity
+                        run_outflow = Math.max(86400 * reach.getDouble("Q" + month + nrPools), run_res_storageInput);
+                        run_outflow = Math.min(run_outflow, run_res_storage);
+                    }
+
+                }
+                
+                
+                double run_RD1_outflow = run_outflow * run_res_prop_RD1;
+                double run_RD2_outflow = run_outflow * run_res_prop_RD2;
+                double run_RG1_outflow = run_outflow * run_res_prop_RG1;
+                double run_RG2_outflow = run_outflow * run_res_prop_RG2;
+                
+                res_storage_RD1.setValue(run_res_storage_RD1 - run_RD1_outflow);
+                res_storage_RD2.setValue(run_res_storage_RD2 - run_RD2_outflow);
+                res_storage_RG1.setValue(run_res_storage_RG1 - run_RG1_outflow);
+                res_storage_RG2.setValue(run_res_storage_RG2 - run_RG2_outflow);
+                
+                res_storage.setValue(run_res_storage - run_outflow);
+                                
+                outflow.setValue(run_outflow);
+                
+                double res_storage_test = res_storage.getValue() - (res_storage_RD1.getValue() + res_storage_RD2.getValue() + res_storage_RG1.getValue() + res_storage_RG2.getValue());
+                
+                
+                if (Math.abs(res_storage_test) > 0.1){
+                    getModel().getRuntime().sendErrorMsg("Reservoir Storage Test not Zero = (" + res_storage_test + ")!");
+                }
+                
+                inRD1.setValue(run_RD1_outflow);
+                inRD2.setValue(run_RD2_outflow);
+                inRG1.setValue(run_RG1_outflow);
+                inRG2.setValue(run_RG2_outflow);
+                
+                
+            }
+
+        }
+
+    }
+
+}
