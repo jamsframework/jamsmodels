ee2fd8779611464aed8a82380f1d26d3e642580e
Nathan Pellerin <nathanpellerin@laposte.net>
2026-01-20 14:20:07 +0000

    Create two lists of HRU ID used to supply irrigation
    git-svn-id: http://svn.geogr.uni-jena.de/svn/jamsmodels/trunk@2232 503a54e1-1403-0410-a754-803ee147b016

---
+++ J2K_Irstea_dev/src/management/InitHRUIrrigationSource.java
@@ -0,0 +1,97 @@
+/*
+ * InitHRUIrrigationSource.java
+ * Created on 20.01.2026, 15:17:17
+ *
+ * This file is part of JAMS
+ * Copyright (C) FSU Jena
+ *
+ * JAMS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * JAMS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JAMS. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package management;
+
+import jams.data.*;
+import jams.model.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+
+
+@JAMSComponentDescription(
+        title = "InitHRUIrrigationSource",
+        author = "Nathan Pellerin",
+        description = "Create two lists of HRU ID used to supply irrigation",
+        date = "2026-01-20",
+        version = "1.0_0")
+
+public class InitHRUIrrigationSource extends JAMSComponent {
+
+    /*
+     *  Component attributes
+     */
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "HRUs list"
+    )
+    public Attribute.EntityCollection hrus;
+    
+    
+    @JAMSVarDescription(
+    access = JAMSVarDescription.AccessType.WRITE,
+    description = "List of all HRU ID used as irrigation sources from GroundWater storage")
+    public Attribute.String irrigation_sources_GW_list;
+    
+    @JAMSVarDescription(
+    access = JAMSVarDescription.AccessType.WRITE,
+    description = "List of all HRU ID used as irrigation sources from Farm Dam storage")
+    public Attribute.String irrigation_source_farmdam_list;
+
+
+    @Override
+public void init() {
+    Set<Integer> gwSources = new HashSet<>();
+    Set<Integer> fdSources = new HashSet<>();
+    
+    for (Attribute.Entity hru : hrus.getEntities()) {
+        double gwsource = hru.getDouble("irrig_source_ID_hru");
+        int gwsource_integer = (int)gwsource;
+
+        if (gwsource_integer > 0){
+            gwSources.add(gwsource_integer);
+        } 
+        
+        double fdsource = hru.getDouble("irrig_source_ID_farmdams");
+        int fdsource_integer = (int)fdsource;
+        
+        if (fdsource_integer > 0){
+            fdSources.add(fdsource_integer);
+        }
+    }
+    String gwlist = gwSources.stream()
+            .map(Object::toString)
+            .collect(Collectors.joining(";"));
+    irrigation_sources_GW_list.setValue(gwlist);  
+    
+    String fdlist = fdSources.stream()
+            .map(Object::toString)
+            .collect(Collectors.joining(";"));
+    irrigation_source_farmdam_list.setValue(fdlist);   
+
+    getModel().getRuntime().println("GW list = " + gwlist+
+            "FD list = " + fdlist);
+
+
+    }
+}
